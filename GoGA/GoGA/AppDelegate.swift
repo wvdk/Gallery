ae87cbee4788073b39eb77ddf9d0403ca1733bf5
Wes Van der Klomp <wes.vdk@icloud.com>
2018-03-01 20:33:48 -0800

    Add table view

--- GoGA/GoGA/AppDelegate.swift
+++ GoGA/GoGA/AppDelegate.swift
@@ -11,13 +11,11 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+    var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = MainViewController()
-        window?.backgroundColor = .red
         window?.makeKeyAndVisible()
         
         return true
--- GoGA/GoGA/MainViewController.swift
+++ GoGA/GoGA/MainViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        
+        tableView.with { it in
+            view.addSubview(it)
+            it.translatesAutoresizingMaskIntoConstraints = false
+            it.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+            it.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+            it.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+            it.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        }
     }
 
 }
+
+public protocol WithProtocol {}
+
+extension NSObject: WithProtocol {}
+
+extension WithProtocol where Self: NSObject {
+    
+    public func with(closure: (Self) -> Void) {
+        closure(self)
+    }
+    
+}
+
 
