618e5741b6af7799da3918b4bf3b47e432bab7ce
Wes Van der Klomp <wes.vdk@icloud.com>
2018-03-03 09:14:57 -0800

    Create the ArtPieceDetailViewController.swift file

---
+++ GoGA/GoGA/ArtPieceDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ArtPieceDetailViewController.swift
+//  GoGA
+//
+//  Created by Wesley Van der Klomp on 3/3/18.
+//  Copyright © 2018 Gallery of Generative Art. All rights reserved.
+//
+
+import UIKit
+
+class ArtPieceDetailViewController: UIViewController {
+    
+}
