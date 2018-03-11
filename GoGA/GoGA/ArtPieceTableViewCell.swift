ba0796ab2f79319ee754d1a099a573dc53e2e1e7
Kristina Gelžinytė <kristina.gelzinyte@gmail.com>
2018-03-11 18:36:28 +0200

    created table view cell for art pieces

---
+++ GoGA/GoGA/ArtPieceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArtPieceTableViewCell.swift
+//  GoGA
+//
+//  Created by Kristina Gelzinyte on 3/11/18.
+//  Copyright © 2018 Gallery of Generative Art. All rights reserved.
+//
+
+import UIKit
+
+class ArtPieceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
