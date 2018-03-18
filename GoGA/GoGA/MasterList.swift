d24c61796ec95848924e5f4023ae3c871ca64283
Kristina Gelžinytė <kristina.gelzinyte@gmail.com>
2018-03-18 14:06:27 +0200

    added new art piece

--- GoGA/GoGA/MasterList.swift
+++ GoGA/GoGA/MasterList.swift
@@ -13,7 +13,8 @@ import UIKit
 struct MasterList {
 
     static let pieces = [
-        Piece(id: "a.736D", author: "Wes Van der Klomp", date: Date(), image: #imageLiteral(resourceName: "a736D.png"), viewController: a736DViewController())
+        Piece(id: "a.736D", author: "Wes Van der Klomp", date: Date(), image: #imageLiteral(resourceName: "a736D.png"), viewController: a736DViewController()),
+        Piece(id: "a.857C", author: "Kristina Gelzinyte", date: Date(), image: #imageLiteral(resourceName: "a857C.png"), viewController: a857CViewController())
     ]
     
 }
