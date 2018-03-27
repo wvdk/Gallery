009b12f8df10029288f794b436cb224c1463d7d0
Wes Van der Klomp <wes.vdk@icloud.com>
2018-03-26 20:40:06 -0700

    Enable ability to automatically open to an art piece on debug builds

--- GoGA/GoGA/AppDelegate.swift
+++ GoGA/GoGA/AppDelegate.swift
@@ -12,11 +12,21 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
+    
+    /// Set this value to whatever you want to open to automatically (only works for debug builds of course).
+    var openTo: ArtPieceDetailViewController? = a565zViewController()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        window?.rootViewController = MainViewController()
+        let mainViewController = MainViewController()
+        window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
+        
+        #if DEBUG
+            if let openTo = openTo {
+                mainViewController.openArtPiece(viewController: openTo)
+            }
+        #endif
         
         print("Here's a fresh ID, if you happen to want one: \(IDGenerator().generateNewArtPieceID())")
         
