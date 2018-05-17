89e9742d6682c8674e8aacd983ed803948cba6ba
Wes Van der Klomp <wes.vdk@icloud.com>
2018-05-17 09:13:51 -0700

    Set a test value in the new firebase db

--- GoGA/GoGA/AppDelegate.swift
+++ GoGA/GoGA/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FirebaseDatabase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
+        
+        let ref = Database.database().reference().child("hello")
+        ref.setValue("world")
         
         let mainViewController = MainViewController()
         window?.rootViewController = mainViewController
