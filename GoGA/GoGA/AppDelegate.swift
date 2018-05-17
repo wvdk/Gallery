665ef235f678bea2827cf7bf06c361e63145f39e
Wes Van der Klomp <wes.vdk@icloud.com>
2018-05-17 09:08:48 -0700

    Set up firebase on app launch

--- GoGA/GoGA/AppDelegate.swift
+++ GoGA/GoGA/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
         
         let mainViewController = MainViewController()
         window?.rootViewController = mainViewController
