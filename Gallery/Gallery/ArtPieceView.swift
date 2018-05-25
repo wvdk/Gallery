6269472e3703b0df4fbc863e86b4d47f084de14b
Wes Van der Klomp <wes.vdk@icloud.com>
2018-05-25 08:41:16 -0700

    create ArtPieceView

---
+++ Gallery/Gallery/ArtPieceView.swift
@@ -0,0 +1,31 @@
+//
+//  ArtPieceView.swift
+//  Gallery
+//
+//  Created by Wesley Van der Klomp on 5/25/18.
+//  Copyright © 2018 Gallery of Generative Art. All rights reserved.
+//
+
+import UIKit
+
+class ArtPieceView: UIView {
+
+    let artPieceMetadata: ArtPiece
+    
+    override init(frame: CGRect) {
+        artPieceMetadata = ArtPiece(id: "", author: "", prettyPublishedDate: "", thumbnailImage: nil, viewController: a857CViewController.self)
+
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented. We're not currently using storyboards in the project. That's probably the issue.")
+    }
+    
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        
+        print("ArtPieceView with id \(artPieceMetadata.id) did move to super view.")
+    }
+    
+}
