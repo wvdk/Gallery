dede0d85bcd8ef149585453d5c7499f2a566a6f1
Wes Van der Klomp <wes.vdk@icloud.com>
2018-06-03 13:11:39 -0700

    Move all the scene stuff into the view

--- Gallery/Gallery/Art Pieces/a.565z/a565zView.swift
+++ Gallery/Gallery/Art Pieces/a.565z/a565zView.swift
@@ -8,17 +8,58 @@
 
 import UIKit
 import ArtKit
+import SceneKit
 
 class a565zView: ArtPieceView {
+    
+    let sceneKitView = SCNView()
+    let scene = SCNScene()
+    let containerNode = SCNNode()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        
+        
         print("initing view")
+        
+        
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        
+        scene.rootNode.addChildNode(cameraNode)
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+        
+        sceneKitView.with { it in
+            it.scene = scene
+            it.allowsCameraControl = false
+            it.backgroundColor = UIColor.white
+            self.addSubview(it)
+            it.frame = self.frame
+        }
+        
+        scene.rootNode.addChildNode(containerNode)
+        
+        createNodeAtCenter()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func createNodeAtCenter() {
+        let plane = SCNPlane(width: 3, height: 3)
+        plane.firstMaterial?.diffuse.contents = UIColor.darkGray
+        let planeNode = SCNNode(geometry: plane)
+        
+        planeNode.position = SCNVector3(x: containerNode.boundingBox.max.x,
+                                        y: containerNode.boundingBox.max.y,
+                                        z: Float(1))
+        
+        let action = SCNAction.scale(by: 10, duration: 10)
+        planeNode.runAction(action)
+        
+        containerNode.addChildNode(planeNode)
     }
     
 }
--- Gallery/Gallery/Art Pieces/a.565z/a565zViewController.swift
+++ Gallery/Gallery/Art Pieces/a.565z/a565zViewController.swift
@@ -12,10 +12,6 @@ import SceneKit
 
 class a565zViewController: ArtPieceDetailViewController {
     
-    let scene = SCNScene()
-    let containerNode = SCNNode()
-    let sceneKitView = SCNView()
-    
     let artPieceView = a565zView()
     
     override func viewDidLoad() {
@@ -27,38 +23,6 @@ class a565zViewController: ArtPieceDetailViewController {
         artPieceView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         artPieceView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         artPieceView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
-        
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        sceneKitView.with { it in
-            it.scene = scene
-            it.allowsCameraControl = false
-            it.backgroundColor = UIColor.white
-            view.addSubview(it)
-            it.frame = view.frame
-        }
-        
-        scene.rootNode.addChildNode(containerNode)
-        
-        createNodeAtCenter()
-    }
-    
-    func createNodeAtCenter() {
-        let plane = SCNPlane(width: 3, height: 3)
-        plane.firstMaterial?.diffuse.contents = UIColor.darkGray
-        let planeNode = SCNNode(geometry: plane)
-        
-        planeNode.position = SCNVector3(x: containerNode.boundingBox.max.x,
-                                        y: containerNode.boundingBox.max.y,
-                                        z: Float(1))
-        
-        let action = SCNAction.scale(by: 10, duration: 10)
-        planeNode.runAction(action)
-        
-        containerNode.addChildNode(planeNode)
     }
     
 }
