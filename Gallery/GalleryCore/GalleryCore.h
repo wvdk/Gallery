ad96e2c58138d5ef1954256e4c9e12194dcb5769
Wes Van der Klomp <wes.vdk@icloud.com>
2018-06-27 08:22:43 -0700

    init gallery core

---
+++ Gallery/GalleryCore/GalleryCore.h
@@ -0,0 +1,19 @@
+//
+//  GalleryCore.h
+//  GalleryCore
+//
+//  Created by Wesley Van der Klomp on 6/27/18.
+//  Copyright © 2018 Gallery of Generative Art. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GalleryCore.
+FOUNDATION_EXPORT double GalleryCoreVersionNumber;
+
+//! Project version string for GalleryCore.
+FOUNDATION_EXPORT const unsigned char GalleryCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GalleryCore/PublicHeader.h>
+
+
