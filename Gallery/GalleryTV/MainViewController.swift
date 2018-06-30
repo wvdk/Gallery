61cab3f13bde964248f37897c0123545e6bc1743
Kristina Gelžinytė <kristina.gelzinyte@gmail.com>
2018-06-30 22:32:09 +0300

    renamed main tvOS view controller

---
+++ Gallery/GalleryTV/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  GalleryTV
+//
+//  Created by Kristina Gelzinyte on 6/30/18.
+//  Copyright © 2018 Gallery of Generative Art. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
