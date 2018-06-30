ad3a8be3b5edc764e8f49b7cb7c2b58c059dd599
Kristina Gelžinytė <kristina.gelzinyte@gmail.com>
2018-06-30 22:09:36 +0300

    tvOS target

---
+++ Gallery/GalleryTV/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  GalleryTV
+//
+//  Created by Kristina Gelzinyte on 6/30/18.
+//  Copyright © 2018 Gallery of Generative Art. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
