ec9c7f34d1176e6c3bc4c33e5ded73d086b618d6
Kristina Gelžinytė <kristina.gelzinyte@gmail.com>
2018-07-01 22:06:51 +0300

    added gallery core tvOS

---
+++ Gallery/GalleryCore tvOS/GalleryCore_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  GalleryCore_tvOS.h
+//  GalleryCore tvOS
+//
+//  Created by Kristina Gelzinyte on 7/1/18.
+//  Copyright © 2018 Gallery of Generative Art. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GalleryCore_tvOS.
+FOUNDATION_EXPORT double GalleryCore_tvOSVersionNumber;
+
+//! Project version string for GalleryCore_tvOS.
+FOUNDATION_EXPORT const unsigned char GalleryCore_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GalleryCore_tvOS/PublicHeader.h>
+
+
