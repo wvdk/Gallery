074f1bb49560730129268cbb4fea0b403f359802
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-07-25 10:23:28 -0700

    refactor SKSpriteNode extension

--- Gallery/ArtKit iOS/SKSpriteNode+Extension.swift
+++ Gallery/ArtKit iOS/SKSpriteNode+Extension.swift
@@ -10,15 +10,20 @@ import SpriteKit
 
 extension SKSpriteNode {
     
-    /// Adds shader to ''SpriteKit'' node and sets shader's size to node's size.
+    /// Adds shader to the `node` and sets shader's size to `node`'s size.
     public func addShader(shader: SKShader) {
-        shader.attributes = [
-            SKAttribute(name: "a_sprite_size", type: .vectorFloat2)
-        ]
+        let width = Float(size.width * UIScreen.main.scale)
+        let height = Float(size.height * UIScreen.main.scale)
+        let nodeSize = vector_float2(width, height)
         
-        let nodeSize = vector_float2(Float(size.width * UIScreen.main.scale),
-                                     Float(size.height * UIScreen.main.scale))
+        let nodeSizeAttributeName = "a_sprite_size"
+        let nodeSizeAttributeValue = SKAttributeValue(vectorFloat2: nodeSize)
+        let nodeSizeAttribute = SKAttribute(name: nodeSizeAttributeName, type: .vectorFloat2)
+        
+        // Creates `a_sprite_size` attribute in shader file, which means the dimension of the `node`.
+        shader.attributes = [nodeSizeAttribute]
+        self.setValue(nodeSizeAttributeValue, forAttribute: nodeSizeAttributeName)
+        
         self.shader = shader
-        self.setValue(SKAttributeValue(vectorFloat2: nodeSize), forAttribute: "a_sprite_size")
     }
 }
