9f2db727bea5198114530bcdc29aeb3383bcc6b7
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-07-25 15:11:44 -0700

    Init UICollectionViewDelegate

---
+++ Gallery/Gallery tvOS/MainViewController+UICollectionViewDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController+UICollectionViewDelegate.swift
+//  Gallery iOS
+//
+//  Created by Kristina Gelzinyte on 7/25/18.
+//  Copyright © 2018 Gallery of Generative Art. All rights reserved.
+//
+
+import UIKit
+
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: collectionViewCellIdentifier, for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+}
