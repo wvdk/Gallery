159918e754ddf3a9779f22d09735922f2290a5b7
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-07-27 12:41:58 -0700

    fixed collection view cell focus appearance

--- Gallery/Gallery tvOS/MainViewController+UICollectionViewDelegate.swift
+++ Gallery/Gallery tvOS/MainViewController+UICollectionViewDelegate.swift
@@ -20,13 +20,33 @@ extension MainViewController: UICollectionViewDelegate {
 
     func collectionView(_ collectionView: UICollectionView, didUpdateFocusIn context: UICollectionViewFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
         
-        if let nextFocusedIndexPath = context.nextFocusedIndexPath, let focusedCell = collectionView.cellForItem(at: nextFocusedIndexPath) {
+        guard let nextFocusedIndexPath = context.nextFocusedIndexPath else { return }
+        
+        if context.previouslyFocusedIndexPath == nil, let focusedCell = collectionView.cellForItem(at: nextFocusedIndexPath) {
+            focusedCell.alpha = 1
+            return
+        }
+        
+        if let previouslyFocusedIndexPath = context.previouslyFocusedIndexPath, nextFocusedIndexPath == previouslyFocusedIndexPath {
+            return
+        }
+        
+        if let nextFocusedItem = context.nextFocusedItem,
+            let focusedCell = collectionView.cellForItem(at: nextFocusedIndexPath),
+            focusedCell.contains(nextFocusedItem),
+            focusedCell.alpha != 1 {
+            
             coordinator.addCoordinatedFocusingAnimations({ (animationContext) in
                 focusedCell.alpha = 1
             }, completion: nil)
         }
         
-        if let previouslyFocusedIndexPath = context.previouslyFocusedIndexPath, let unfocusedCell = collectionView.cellForItem(at: previouslyFocusedIndexPath) {
+        if let previouslyFocusedItem = context.previouslyFocusedItem,
+            let previouslyFocusedIndexPath = context.previouslyFocusedIndexPath,
+            let unfocusedCell = collectionView.cellForItem(at: previouslyFocusedIndexPath),
+            unfocusedCell.contains(previouslyFocusedItem),
+            unfocusedCell.alpha != 0.5 {
+            
             coordinator.addCoordinatedUnfocusingAnimations({ (animationContext) in
                 unfocusedCell.alpha = 0.5
             }, completion: nil)
