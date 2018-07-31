5f06ba4078b14abb1305498ff983bd827cf70327
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-07-31 14:56:37 -0700

    added header view to the main screen

--- Gallery/Gallery tvOS/MainViewController.swift
+++ Gallery/Gallery tvOS/MainViewController.swift
@@ -21,29 +21,40 @@ class MainViewController: UIViewController {
 
     var collectionView: UICollectionView?
     
+    let headerView = UIView()
+    
     // MARK: - Lifecycle functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupCollectionView()
+        let headerView = setupHeaderView()
 
         guard let collectionView = self.collectionView else { return }
         
         self.view.addSubview(collectionView)
-        
+        self.view.addSubview(headerView)
+
         collectionView.translatesAutoresizingMaskIntoConstraints = false
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+
         collectionView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: collectionViewTopConstrain).isActive = true
         collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -collectionViewBottomConstrain).isActive = true
         collectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
         collectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        
+        headerView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        headerView.bottomAnchor.constraint(equalTo: collectionView.topAnchor).isActive = true
+        headerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        headerView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
         
 //        let menuTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.back))
 //        menuTapRecognizer.allowedPressTypes = [NSNumber(value: UIPressType.menu.rawValue)];
 //        view.addGestureRecognizer(menuTapRecognizer)
     }
     
-    // MARK: - Collection view setup
+    // MARK: - View setup
     
     private func setupCollectionView() {
         let layout = UICollectionViewFlowLayout()
@@ -64,6 +75,32 @@ class MainViewController: UIViewController {
         collectionView?.remembersLastFocusedIndexPath = true
         
 //        collectionView?.backgroundColor = .black
+    }
+    
+    private func setupHeaderView() -> UIView {
+        let headerLabel = HeadlineLabel(isFontBold: false)
+        headerLabel.text = "Premium"
+        
+        let separatorView = UIView()
+        separatorView.backgroundColor = .gray
+        
+        let headerContainerView = UIView()
+        
+        headerContainerView.addSubview(headerLabel)
+        headerContainerView.addSubview(separatorView)
+
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        separatorView.translatesAutoresizingMaskIntoConstraints = false
+
+        separatorView.bottomAnchor.constraint(equalTo: headerContainerView.bottomAnchor).isActive = true
+        separatorView.leadingAnchor.constraint(equalTo: headerContainerView.leadingAnchor, constant: 180).isActive = true
+        separatorView.trailingAnchor.constraint(equalTo: headerContainerView.trailingAnchor, constant: -180).isActive = true
+        separatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        headerLabel.leadingAnchor.constraint(equalTo: separatorView.leadingAnchor).isActive = true
+        headerLabel.bottomAnchor.constraint(equalTo: separatorView.topAnchor, constant: -10).isActive = true
+        
+        return headerContainerView
     }
     
     // MARK: - App control
