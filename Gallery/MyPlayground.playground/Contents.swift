c497970cfb0a9a5bb442c100877a15d892130707
Wes Van der Klomp <wes.vdk@icloud.com>
2018-08-01 08:46:19 -0700

    movement

--- Gallery/MyPlayground.playground/Contents.swift
+++ Gallery/MyPlayground.playground/Contents.swift
@@ -10,13 +10,21 @@ extension UIView {
         return NSKeyedUnarchiver.unarchiveObject(with: NSKeyedArchiver.archivedData(withRootObject: self)) as! T
     }
     
-    func loopInSuperview(duplicationCount: Int) {
+    func loopInSuperview(duplicationCount: Int, moveHorizontallyWithIncrement xMovement: Int = 0, moveVerticallyWithIncrement yMovement: Int = 0) {
         /// We start with an index of 2 - which seems weird but if very deliberate. If we use a starting index of 0, then multiply the origin points by that 0 index, it would bring the new view to the top left corner. Similarly if we start with 1, it will cover the original view.
         let startingIndex = 2
         let endingIndex = duplicationCount + startingIndex
         for i in startingIndex..<endingIndex {
             let newView = self.copyView()
-            newView.frame = CGRect(x: frame.origin.x * CGFloat(i), y: frame.origin.y * CGFloat(i), width: frame.width, height: frame.height)
+            
+            
+            // Move
+            newView.frame = CGRect(x: frame.origin.x + CGFloat((i - 1) * xMovement),
+                                   y: frame.origin.y + CGFloat((i - 1) * yMovement),
+                                   width: frame.width,
+                                   height: frame.height)
+            
+            
             
             self.superview?.addSubview(newView)
             newView.backgroundColor = .blue
@@ -47,7 +55,7 @@ class MyViewController : UIViewController {
         
         view.addSubview(box)
         
-        box.loopInSuperview(duplicationCount: 3)
+        box.loopInSuperview(duplicationCount: 3, moveHorizontallyWithIncrement: 60, moveVerticallyWithIncrement: -4)
         
     }
     
