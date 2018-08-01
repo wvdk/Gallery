b0ff43396786caadfbf46b7a5789b9317a498a5f
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-07-31 19:14:04 -0700

    added focus animation to tab bar items

--- Gallery/Gallery tvOS/TabBarController.swift
+++ Gallery/Gallery tvOS/TabBarController.swift
@@ -16,25 +16,38 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let membershipViewController = UIViewController()
-        membershipViewController.view.backgroundColor = .blue
-        membershipViewController.tabBarItem = UITabBarItem(title: "Membership", image: nil, tag: 0)
-        membershipViewController.tabBarItem.titlePositionAdjustment.horizontal = -100
-        
         let allArtPieceViewController = AllArtPieceViewController()
         allArtPieceViewController.tabBarItem = UITabBarItem(title: "ALL", image: nil, tag: 1)
-        
+        allArtPieceViewController.tabBarItem.setTitleTextAttributes([NSAttributedStringKey.font: UIFont.systemFont(ofSize: 30)], for: .normal)
+     
         let purchasedArtPieceViewController = UIViewController()
         purchasedArtPieceViewController.view.backgroundColor = .red
         purchasedArtPieceViewController.tabBarItem = UITabBarItem(title: "YOURS", image: nil, tag: 2)
+        purchasedArtPieceViewController.tabBarItem.setTitleTextAttributes([NSAttributedStringKey.font: UIFont.systemFont(ofSize: 30)], for: .normal)
         
-        let artPiecePresentatioViewController = UIViewController()
-        artPiecePresentatioViewController.view.backgroundColor = .black
-        artPiecePresentatioViewController.tabBarItem = UITabBarItem(title: "Shuffle", image: nil, tag: 3)
+//        let membershipViewController = UIViewController()
+//        membershipViewController.view.backgroundColor = .blue
+//        membershipViewController.tabBarItem = UITabBarItem(title: "Membership", image: nil, tag: 0)
+//
+//        let artPiecePresentatioViewController = UIViewController()
+//        artPiecePresentatioViewController.view.backgroundColor = .black
+//        artPiecePresentatioViewController.tabBarItem = UITabBarItem(title: "Shuffle", image: nil, tag: 3)
         
-        let tabBarViewControllerList = [membershipViewController, allArtPieceViewController, purchasedArtPieceViewController, artPiecePresentatioViewController]
+//        let tabBarViewControllerList = [membershipViewController, allArtPieceViewController, purchasedArtPieceViewController, artPiecePresentatioViewController]
+        
+        let tabBarViewControllerList = [allArtPieceViewController, purchasedArtPieceViewController]
         self.viewControllers = tabBarViewControllerList
         
-        self.selectedIndex = 1
+        self.selectedIndex = 0
+    }
+    
+    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        guard let nextView = context.nextFocusedView, self.tabBar.contains(nextView) else { return }
+        
+        coordinator.addCoordinatedFocusingAnimations({ (animationContext) in
+            nextView.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        }, completion: {
+            nextView.transform = CGAffineTransform.identity
+        })
     }
 }
