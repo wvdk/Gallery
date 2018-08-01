a442d270e7b09b7a265029aa02817440c77f5f84
Wes Van der Klomp <wes.vdk@icloud.com>
2018-08-01 08:23:17 -0700

    basic proof of concept

--- Gallery/MyPlayground.playground/Contents.swift
+++ Gallery/MyPlayground.playground/Contents.swift
@@ -4,7 +4,58 @@ import UIKit
 import PlaygroundSupport
 import ArtKit_iOS
 
+extension UIView {
+    
+    func copyView<T: UIView>() -> T {
+        return NSKeyedUnarchiver.unarchiveObject(with: NSKeyedArchiver.archivedData(withRootObject: self)) as! T
+    }
+    
+    func loopInSuperview() {
+        for i in 2...5 {
+            // We start with an index of 2 - which seems weird but if very deliberate. If we use a starting index of 0, then multiply the origin points by that 0 index, it would bring the new view to the top left corner. Similarly if we start with 1, it will cover the original view.
+            
+            let newView = self.copyView()
+            newView.frame = CGRect(x: frame.origin.x * CGFloat(i), y: frame.origin.y * CGFloat(i), width: frame.width, height: frame.height)
+            
+            self.superview?.addSubview(newView)
+            newView.backgroundColor = .blue
+        }        
+        
+    }
+    
+    
+    
+}
+
+
+
 class MyViewController : UIViewController {
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        let box = UIView()
+        box.frame = CGRect(x: 50, y: 50, width: 50, height: 50)
+        box.backgroundColor = .red
+        
+        view.addSubview(box)
+        
+        box.loopInSuperview()
+        
+    }
+    
+    
+    
+    
+    
+    
+    
     
     override func loadView() {
         let view = UIView()
@@ -17,6 +68,8 @@ class MyViewController : UIViewController {
         
         view.addSubview(label)
         self.view = view
+        
+        
     }
     
 }
