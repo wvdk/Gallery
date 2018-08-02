d33452cfadb64782cbdcfe2ac66de3e8d8e64dc9
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-08-01 17:57:25 -0700

    new collection view

--- Gallery/Gallery tvOS/NotFeaturedArtPieceViewController.swift
+++ Gallery/Gallery tvOS/NotFeaturedArtPieceViewController.swift
@@ -10,9 +10,54 @@ import UIKit
 
 class NotFeaturedArtPieceViewController: UIViewController {
 
+    // MARK: - Properties
+
+    var collectionView: UICollectionView?
+    
+    // MARK: - Lifecycle functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupCollectionView()
+        
+        guard let collectionView = self.collectionView else { return }
 
-        self.view.backgroundColor = .black
+        self.view.addSubview(collectionView)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        collectionView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+
+        self.view.backgroundColor = .red
+    }
+    
+    // MARK: - View setup
+    
+    private func setupCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        
+        guard let collectionView = collectionView else { return }
+        
+        collectionView.register(FeaturedArtPieceCollectionViewCell.self, forCellWithReuseIdentifier: FeaturedArtPieceCollectionViewCell.identifier)
+        collectionView.decelerationRate = UIScrollViewDecelerationRateNormal
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+//        collectionView.isScrollEnabled = false
+        collectionView.allowsSelection = true
+        collectionView.allowsMultipleSelection = false
+        
+        collectionView.remembersLastFocusedIndexPath = true
+        
+        collectionView.selectCell(at: collectionView.firstCellIndex)
     }
 }
