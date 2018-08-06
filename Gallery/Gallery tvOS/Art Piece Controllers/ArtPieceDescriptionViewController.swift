719d210e4f38ebdbb38149460bb397680254126c
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-08-06 10:57:52 -0700

    added label to description controller

--- Gallery/Gallery tvOS/Art Piece Controllers/ArtPieceDescriptionViewController.swift
+++ Gallery/Gallery tvOS/Art Piece Controllers/ArtPieceDescriptionViewController.swift
@@ -10,26 +10,17 @@ import GalleryCore_tvOS
 
 class ArtPieceDescriptionViewController: UIViewController {
 
-    /// <#Description#>
-    var artPieceMetadata: ArtMetadata
+    // MARK: - Properties
     
-    /// <#Description#>
-    override func viewDidLoad() {
-        super.viewDidLoad()
-                
-        let backgroundView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
-        view.addSubview(backgroundView)
-        backgroundView.translatesAutoresizingMaskIntoConstraints = false
-        backgroundView.constraint(edgesTo: view)
-        
-
-        
-        
-    }
+    private var artPieceMetadata: ArtMetadata
     
-    /// <#Description#>
-    ///
-    /// - Parameter metadata:
+    private let authorNameLabel = HeadlineLabel(color: .white, isFontBold: true)
+    private let titleLabel = BodyLabel(color: .white)
+    private let dateLabel = BodyLabel(color: .white)
+    private let descriptionLabel = BodyLabel(color: .white)
+    
+    // MARK: - Initialization
+    
     init(artMetadata: ArtMetadata) {
         self.artPieceMetadata = artMetadata
         
@@ -38,5 +29,59 @@ class ArtPieceDescriptionViewController: UIViewController {
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Lifecycle functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.textAlignment = .justified
+        
+        updateDescriptionText()
+        
+        let backgroundView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
+        let stackView = setupDescriptionStackView()
+        
+        view.addSubview(backgroundView)
+        view.addSubview(stackView)
+        
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        backgroundView.constraint(edgesTo: view)
+
+        stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        stackView.bottomAnchor.constraint(lessThanOrEqualTo: view.bottomAnchor, constant: -100).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 500).isActive = true
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+
+    // MARK: - Stackview setup
+    
+    private func setupDescriptionStackView() -> UIStackView {
+        let labelViews = [authorNameLabel, titleLabel, dateLabel, descriptionLabel]
+        let stackView = UIStackView(arrangedSubviews: labelViews)
+        
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 30
+        stackView.alignment = .trailing
+        
+        return stackView
+    }
+    
+    private func updateDescriptionText() {
+        authorNameLabel.text = "\(artPieceMetadata.author)"
+        dateLabel.text = "\(artPieceMetadata.prettyPublishedDate)"
+        titleLabel.text = "\(artPieceMetadata.id)"
+        
+        if let description = artPieceMetadata.description {
+            descriptionLabel.text = description
+        } else {
+            descriptionLabel.text = "Lorem ipsum dolor sit amet, ligula suspendisse nulla pretium, rhoncus tempor fermentum, enim integer ad vestibulum volutpat. Nisl rhoncus turpis est, vel elit, congue wisi enim nunc ultricies sit, magna tincidunt. Maecenas aliquam. gna tincidunt. Maecenas aliquam tincidunt. Maecenas aliquam \nLorem ipsum dolor sit amet, ligula suspendisse nulla pretium, rhoncus tempor fermentum, enim integer ad vestibulum volutpat. Nisl rhoncus turpis est, vel elit, congue wisi enim nunc ultricies sit, magna tincidunt. Maecenas aliquam. gna tincidunt. Maecenas aliquam tincidunt. Maecenas aliquamorem ipsum dolor sit amet, ligula suspendisse nulla pretium, rhoncus tempor fermentum, enim integer ad vestibulum volutpat. Nisl rhoncus turpis est, vel elit, congue wisi enim nunc ultricies sit, magna tincidunt. Maecenas aliquam. gna tincidunt. Maecenas aliquam tincidunt. Maecenas aliquam \nLorem ipsum dolor sit amet, ligula suspendisse nulla pretium, rhoncus tempor fermentum, enim integer ad vestibulum volutpat. Nisl rhoncus turpis est, vel elit, congue wisi enim nunc ultricies sit, magna tincidunt. Maecenas aliquam. gna tincidunt. Maecenas aliquam tincidunt. Maecenas aliquamorem ipsum dolor sit amet, ligula suspendisse nulla pretium, rhoncus tempor fermentum, enim integer ad vestibulum volutpat. Nisl rhoncus turpis est, vel elit, congue wisi enim nunc ultricies sit, magna tincidunt. Maecenas aliquam. gna tincidunt. Maecenas aliquam tincidunt. Maecenas aliquam \nLorem ipsum dolor sit amet, ligula suspendisse nulla pretium, rhoncus tempor fermentum, enim integer ad vestibulum volutpat. Nisl rhoncus turpis est, vel elit, congue wisi enim nunc ultricies sit, magna tincidunt. Maecenas aliquam. gna tincidunt. Maecenas aliquam tincidunt. Maecenas aliquam volutpat. Nisl rhoncus turpis est, vel elit, congue wisi enim nunc ultricies sit, magna tincidunt. Maecenas aliquam. gna tincidunt. Maecenas aliquam tincidunt. Maecenas aliquam \nLorem ipsum dolor sit amet, ligula suspendisse nulla pretium, rhoncus tempor fermentum, enim integer ad vestibulum volutpat. Nisl rhoncus turpis est, vel elit, congue wisi enim nunc ultricies sit, magna tincidunt. Maecenas aliquam. gna tincidunt. Maecenas aliquam tincidunt. Maecenas aliquamorem ipsum dolor sit amet, ligula suspendisse nulla pretium, rhoncus tempor fermentum, enim integer ad vestibulum volutpat. Nisl rhoncus turpis est, vel elit, congue wisi enim nunc ultricies sit, magna tincidunt. Maecenas aliquam. gna tincidunt. Maecenas aliquam tincidunt. Maecenas aliquam \nLorem ipsum dolor sit amet, ligula suspendisse nulla pretium, rhoncus tempor fermentum, enim integer ad vestibulum volutpat. Nisl rhoncus turpis est, vel elit, congue wisi enim nunc ultricies sit, magna tincidunt. Maecenas aliquam. gna tincidunt. Maecenas aliquam tincidunt. Maecenas aliquamorem ipsum dolor sit amet, ligula suspendisse nulla pretium, rhoncus tempor fermentum, enim integer ad vestibulum volutpat. Nisl rhoncus turpis est, vel elit, congue wisi enim nunc ultricies sit, magna tincidunt. Maecenas aliquam. gna tincidunt. Maecenas aliquam tincidunt. Maecenas aliquam \nLorem ipsum dolor sit amet, ligula suspendisse nulla pretium, rhoncus tempor fermentum, enim integer ad vestibulum volutpat. Nisl rhoncus turpis est, vel elit, congue wisi enim nunc ultricies sit, magna tincidunt. Maecenas aliquam. gna tincidunt. Maecenas aliquam tincidunt. Maecenas aliquam"
+        }
     }
 }
