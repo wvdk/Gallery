dacd3382f32044638e5b999b47c5bbc57d480bc4
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-08-08 11:24:54 -0700

    allArtPiecesViewController as main vc

--- Gallery/Gallery tvOS/AppDelegate.swift
+++ Gallery/Gallery tvOS/AppDelegate.swift
@@ -15,8 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        let tabBarController = TabBarController()
-        window?.rootViewController = tabBarController
+        let allArtPiecesViewController = AllArtPiecesViewController()
+        window?.rootViewController = allArtPiecesViewController
         window?.makeKeyAndVisible()
         
         return true
