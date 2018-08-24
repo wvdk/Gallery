7515f760cbdde12f9339e7d184dfb0d97ab926da
Wes Van der Klomp <wes.vdk@icloud.com>
2018-08-24 08:05:07 -0700

    reworking A656x

--- Gallery/GalleryCore iOS/Art Pieces/A565zView.swift
+++ Gallery/GalleryCore iOS/Art Pieces/A565zView.swift
@@ -16,55 +16,22 @@ import ArtKit_tvOS
 #endif
 
 public class A565zView: ArtView {
-    
-    // MARK: - Properties
-    
-    private let sceneKitView = SCNView()
-    private let scene = SCNScene()
-    private let containerNode = SCNNode()
-    
-    // MARK: - Initialization
 
     public required init(frame: CGRect, artPieceMetadata: ArtMetadata) {
         super.init(frame: frame, artPieceMetadata: artPieceMetadata)
         
-        tag = 123
+        backgroundColor = .white
         
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        
-        scene.rootNode.addChildNode(cameraNode)
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        addSubview(sceneKitView)
-        sceneKitView.translatesAutoresizingMaskIntoConstraints = false
-        sceneKitView.constraint(edgesTo: self)
-        
-        sceneKitView.scene = scene
-        sceneKitView.allowsCameraControl = false
-        
-        scene.rootNode.addChildNode(containerNode)
-        
-        createNodeAtCenter()        
+        let box = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        addSubview(box)
+        box.backgroundColor = .white
+        box.layer.borderColor = UIColor.blue.cgColor
+        box.layer.borderWidth = 1.0
+        box.loopInSuperview(duplicationCount: 3, with: [UIView.LoopingOptions.moveHorizontallyWithIncrement(200)])
     }
     
     public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: - Node creation
-    
-    private func createNodeAtCenter() {
-        let plane = SCNPlane(width: 3, height: 3)
-        plane.firstMaterial?.diffuse.contents = UIColor.darkGray
-        
-        let planeNode = SCNNode(geometry: plane)
-        planeNode.position = SCNVector3(x: containerNode.boundingBox.max.x,
-                                        y: containerNode.boundingBox.max.y,
-                                        z: Float(1))
-        
-        let action = SCNAction.scale(by: 10, duration: 10)
-        planeNode.runAction(action)
-        containerNode.addChildNode(planeNode)
-    }
 }
