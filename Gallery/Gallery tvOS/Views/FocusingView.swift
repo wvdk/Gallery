fdcd871966910235a17161aed4eb41a89ee78d35
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-09-06 14:30:03 -0700

    Refactoring

--- Gallery/Gallery tvOS/Views/FocusingView.swift
+++ Gallery/Gallery tvOS/Views/FocusingView.swift
@@ -201,3 +201,23 @@ class FocusingView: UIView {
         self.artPieceViewParralaxMotionEffect = nil
     }
 }
+
+/// The object that acts as the delegate of the focusing view.
+///
+/// The delegate must adopt the FocusingViewDelegate protocol.
+///
+/// The delegate object is responsible for managing the focusing view focus state.
+protocol FocusingViewDelegate: class {
+    
+    /// Tells the delegate that the view became focused.
+    ///
+    /// - Parameters:
+    ///     - focusingView: An object informing the delegate that view became focused.
+    func focusingViewDidBecomeFocused(_ focusingView: FocusingView)
+    
+    /// Tells the delegate that the view resign the focus.
+    ///
+    /// - Parameters:
+    ///     - focusingView: An object informing the delegate that view resign focus.
+    func focusingViewDidResignedFocus(_ focusingView: FocusingView)
+}
