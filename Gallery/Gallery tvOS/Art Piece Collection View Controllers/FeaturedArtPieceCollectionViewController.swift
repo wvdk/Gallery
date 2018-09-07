5d6a7cc886710b2c0e2235bd870c960c4911471b
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-09-07 14:33:37 -0700

    Rotating lines to the header view and some refactoring

--- Gallery/Gallery tvOS/Art Piece Collection View Controllers/FeaturedArtPieceCollectionViewController.swift
+++ Gallery/Gallery tvOS/Art Piece Collection View Controllers/FeaturedArtPieceCollectionViewController.swift
@@ -8,60 +8,74 @@
 
 import UIKit
 
-/// A subclass of `UIViewController` which contains a featured art piece collection view.
+/// A subclass of `UIViewController` which contains a featured art piece collection view with a horizontal scrolling direction.
 class FeaturedArtPieceCollectionViewController: UIViewController {
    
     // MARK: - Properties
     
     /// The object that acts as the delegate of the `ArtPieceCollectionViewControllerDelegate`.
-    weak var delegate: ArtPieceCollectionViewControllerDelegate?
+    weak var delegate: ArtPieceCollectionViewControllerDelegate? = nil
     
-    var collectionView: DisabledFocusCollectionView?
-    
-    private let headerView = UIView()
-    
-    // MARK: - Lifecycle functions
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    /// Sets up rotating line view of 60 duplicates.
+    private var rotatingLineView: UIView {
+        let view = UIView()
         
-        let headerView = setupHeaderView()
+        let line = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: 250))
+        view.addSubview(line)
         
-        view.addSubview(headerView)
+        line.layer.shadowOffset = CGSize(width: 1, height: 1)
+        line.layer.shadowColor = UIColor(r: 0, g: 0, b: 0, alpha: 0.5).cgColor
         
-        headerView.translatesAutoresizingMaskIntoConstraints = false
-
-        headerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        headerView.heightAnchor.constraint(equalToConstant: 140).isActive = true
-
-        setupCollectionView()
+        line.transform = CGAffineTransform(rotationAngle: -.pi / 4)
+        line.backgroundColor = UIColor(r: 173, g: 173, b: 173, alpha: 0.65)
+        line.alpha = 0.05
         
-        guard let collectionView = self.collectionView else { return }
+        line.loopInSuperview(duplicationCount: 60, with: [
+            .rotateByDegrees(-0.06),
+            .moveHorizontallyWithIncrement(30),
+            .updateOpacityIncreasingly
+            ]
+        )
         
-        view.addSubview(collectionView)
+        for subview in view.subviews {
+            subview.rotate(duration: 50.0)
+        }
         
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        
-        collectionView.topAnchor.constraint(equalTo: headerView.bottomAnchor).isActive = true
-        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        return view
     }
     
-    // MARK: - Views setup
+    /// Sets up header which consist of title "Gallery of Generative Art" and rotating lines.
+    private var headerView: UIView {
+        let headerContainerView = UIView()
+        
+        let headerLabel = UILabel()
+        headerLabel.font = UIFont.systemFont(ofSize: 35)
+        headerLabel.textColor = UIColor(r: 173, g: 173, b: 173, alpha: 0.5)
+        headerLabel.text = "Gallery of Generative Art"
+        headerContainerView.addSubview(headerLabel)
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        headerLabel.centerYAnchor.constraint(equalTo: headerContainerView.centerYAnchor).isActive = true
+        headerLabel.trailingAnchor.constraint(equalTo: headerContainerView.trailingAnchor, constant: -view.frame.size.height * 50 / 1119).isActive = true
+        
+        let lineView = rotatingLineView
+        headerContainerView.addSubview(lineView)
+        lineView.translatesAutoresizingMaskIntoConstraints = false
+        lineView.leadingAnchor.constraint(equalTo: headerContainerView.leadingAnchor, constant: view.frame.size.width * 300 / 1920).isActive = true
+        lineView.trailingAnchor.constraint(equalTo: headerContainerView.trailingAnchor, constant: view.frame.size.width * 280 / 1920).isActive = true
+        lineView.topAnchor.constraint(equalTo: headerContainerView.topAnchor, constant: -view.frame.size.height * 50 / 1119).isActive = true
+        lineView.bottomAnchor.constraint(equalTo: headerContainerView.topAnchor, constant: view.frame.size.height * 200 / 1119).isActive = true
+        
+        return headerContainerView
+    }
     
-    /// Sets up `UICollectionView` with horizontal scrolling direction.
-    private func setupCollectionView() {
+    /// Sets up `UICollectionView` with a horizontal scrolling direction.
+    private var collectionView: DisabledFocusCollectionView {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         layout.minimumLineSpacing = 0
         layout.minimumInteritemSpacing = 0
         
-        collectionView = DisabledFocusCollectionView(frame: view.frame, collectionViewLayout: layout)
-        
-        guard let collectionView = collectionView else { return }
+        let collectionView = DisabledFocusCollectionView(frame: view.frame, collectionViewLayout: layout)
         
         collectionView.register(ArtPieceCollectionViewCell.self, forCellWithReuseIdentifier: ArtPieceCollectionViewCell.identifier)
         collectionView.decelerationRate = UIScrollViewDecelerationRateNormal
@@ -71,25 +85,33 @@ class FeaturedArtPieceCollectionViewController: UIViewController {
         collectionView.isScrollEnabled = false
         collectionView.allowsSelection = true
         collectionView.allowsMultipleSelection = false
-
+        
         collectionView.remembersLastFocusedIndexPath = true
         
         collectionView.selectCell(at: collectionView.firstCellIndex)
+        
+        return collectionView
     }
     
-    /// Sets up header which consist of title "Gallery of Generative Art".
-    private func setupHeaderView() -> UIView {
-        let headerLabel = HeadlineLabel(isFontBold: false)
-        headerLabel.text = "Gallery of Generative Art"
+    // MARK: - Lifecycle functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        let headerContainerView = UIView()
-        headerContainerView.addSubview(headerLabel)
-        
-        headerLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        headerLabel.centerYAnchor.constraint(equalTo: headerContainerView.centerYAnchor).isActive = true
-        headerLabel.trailingAnchor.constraint(equalTo: headerContainerView.trailingAnchor, constant: -view.frame.size.height * 50 / 1119).isActive = true
-        
-        return headerContainerView
+        let header = headerView
+        view.addSubview(header)
+        header.translatesAutoresizingMaskIntoConstraints = false
+        header.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        header.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        header.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        header.heightAnchor.constraint(equalToConstant: 140).isActive = true
+
+        let collection = collectionView
+        view.addSubview(collection)
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        collection.topAnchor.constraint(equalTo: header.bottomAnchor).isActive = true
+        collection.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        collection.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        collection.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
 }
