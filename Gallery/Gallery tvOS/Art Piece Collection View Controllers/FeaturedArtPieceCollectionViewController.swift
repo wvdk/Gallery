27abe388da5d7536cc5e27a69db52064694afa01
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-09-10 14:09:07 -0700

    Rotating line header

--- Gallery/Gallery tvOS/Art Piece Collection View Controllers/FeaturedArtPieceCollectionViewController.swift
+++ Gallery/Gallery tvOS/Art Piece Collection View Controllers/FeaturedArtPieceCollectionViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-/// A subclass of `UIViewController` which contains a featured art piece collection view.
+/// A subclass of `UIViewController` which contains a featured art piece collection view with a horizontal scrolling direction.
 class FeaturedArtPieceCollectionViewController: UIViewController {
    
     // MARK: - Properties
     
     /// The object that acts as the delegate of the `ArtPieceCollectionViewControllerDelegate`.
-    weak var delegate: ArtPieceCollectionViewControllerDelegate?
+    weak var delegate: ArtPieceCollectionViewControllerDelegate? = nil
+    
+    /// Returns tag id integer number for rotating line superview.
+    private let rotatingLineViewTag = 10928
     
     /// Sets up `UICollectionView` with horizontal scrolling direction.
     private var verticalFlowCollectionView: UICollectionView {
@@ -41,17 +44,56 @@ class FeaturedArtPieceCollectionViewController: UIViewController {
     }
     
     /// Sets up header which consist of title "Gallery of Generative Art".
-    private var headerView: UIView {
-        let headerLabel = HeadlineLabel(isFontBold: false)
-        headerLabel.text = "Gallery of Generative Art"
-        
+    private lazy var headerView: UIView = {
         let headerContainerView = UIView()
+
+        let headerLabel = UILabel()
+        headerLabel.font = UIFont.systemFont(ofSize: 35)
+        headerLabel.textColor = UIColor(r: 173, g: 173, b: 173, alpha: 0.5)
+        headerLabel.text = "Gallery of Generative Art"
         headerContainerView.addSubview(headerLabel)
         headerLabel.translatesAutoresizingMaskIntoConstraints = false
         headerLabel.centerYAnchor.constraint(equalTo: headerContainerView.centerYAnchor).isActive = true
         headerLabel.trailingAnchor.constraint(equalTo: headerContainerView.trailingAnchor, constant: -view.frame.size.height * 50 / 1119).isActive = true
         
+        let lineView = rotatingLineView
+        lineView.tag = rotatingLineViewTag
+        headerContainerView.addSubview(lineView)
+        lineView.translatesAutoresizingMaskIntoConstraints = false
+        lineView.leadingAnchor.constraint(equalTo: headerContainerView.leadingAnchor, constant: view.frame.size.width * 300 / 1920).isActive = true
+        lineView.trailingAnchor.constraint(equalTo: headerContainerView.trailingAnchor, constant: view.frame.size.width * 280 / 1920).isActive = true
+        lineView.topAnchor.constraint(equalTo: headerContainerView.topAnchor, constant: -view.frame.size.height * 50 / 1119).isActive = true
+        lineView.bottomAnchor.constraint(equalTo: headerContainerView.topAnchor, constant: view.frame.size.height * 200 / 1119).isActive = true
+        
         return headerContainerView
+    }()
+    
+    /// Sets up rotating line view of 60 duplicates.
+    private var rotatingLineView: UIView {
+        let view = UIView()
+        
+        let line = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: 250))
+        view.addSubview(line)
+        
+        line.layer.shadowOffset = CGSize(width: 1, height: 1)
+        line.layer.shadowColor = UIColor(r: 0, g: 0, b: 0, alpha: 0.5).cgColor
+        
+        line.transform = CGAffineTransform(rotationAngle: -.pi / 4)
+        line.backgroundColor = UIColor(r: 173, g: 173, b: 173, alpha: 0.65)
+        line.alpha = 0.05
+        
+        line.loopInSuperview(duplicationCount: 60, with: [
+            .rotateByDegrees(-0.06),
+            .moveHorizontallyWithIncrement(30),
+            .updateOpacityIncreasingly
+            ]
+        )
+        
+        for subview in view.subviews {
+            subview.rotate(duration: 50.0)
+        }
+        
+        return view
     }
     
     // MARK: - Lifecycle functions
@@ -59,20 +101,29 @@ class FeaturedArtPieceCollectionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let header = headerView
-        view.addSubview(header)
-        header.translatesAutoresizingMaskIntoConstraints = false
-        header.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        header.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        header.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        header.heightAnchor.constraint(equalToConstant: 140).isActive = true
+        view.addSubview(headerView)
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        headerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        headerView.heightAnchor.constraint(equalToConstant: 140).isActive = true
 
         let collectionView = verticalFlowCollectionView
         view.addSubview(collectionView)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.topAnchor.constraint(equalTo: header.bottomAnchor).isActive = true
+        collectionView.topAnchor.constraint(equalTo: headerView.bottomAnchor).isActive = true
         collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let lineView = headerView.subviews.first(where: { $0.tag == rotatingLineViewTag }) {
+            for subview in lineView.subviews {
+                subview.rotate(duration: 30.0)
+            }
+        }
     }
 }
