828b71e7310f7d8009b775b228c0f4190b4a7dac
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-09-10 17:46:52 -0700

    Refactoring

--- Gallery/Gallery tvOS/Art Piece Collection View Controllers/FeaturedArtPieceCollectionViewController.swift
+++ Gallery/Gallery tvOS/Art Piece Collection View Controllers/FeaturedArtPieceCollectionViewController.swift
@@ -16,9 +16,6 @@ class FeaturedArtPieceCollectionViewController: UIViewController {
     /// The object that acts as the delegate of the `ArtPieceCollectionViewControllerDelegate`.
     weak var delegate: ArtPieceCollectionViewControllerDelegate? = nil
     
-    /// Returns tag id integer number for rotating line superview.
-    private let rotatingLineViewTag = 10928
-    
     /// Sets up `UICollectionView` with horizontal scrolling direction.
     private var verticalFlowCollectionView: UICollectionView {
         let layout = UICollectionViewFlowLayout()
@@ -43,86 +40,13 @@ class FeaturedArtPieceCollectionViewController: UIViewController {
         return collectionView
     }
     
-    /// Sets up header which consist of title "Gallery of Generative Art".
-    private lazy var headerView: UIView = {
-        let headerContainerView = UIView()
-
-        let lineView = rotatingLineView
-        lineView.tag = rotatingLineViewTag
-        headerContainerView.addSubview(lineView)
-        lineView.translatesAutoresizingMaskIntoConstraints = false
-        lineView.leadingAnchor.constraint(equalTo: headerContainerView.leadingAnchor, constant: view.frame.size.width * 300 / 1920).isActive = true
-        lineView.trailingAnchor.constraint(equalTo: headerContainerView.trailingAnchor, constant: view.frame.size.width * 280 / 1920).isActive = true
-        lineView.topAnchor.constraint(equalTo: headerContainerView.topAnchor, constant: -view.frame.size.height * 50 / 1119).isActive = true
-        lineView.bottomAnchor.constraint(equalTo: headerContainerView.topAnchor, constant: view.frame.size.height * 200 / 1119).isActive = true
-        
-        let headerLabel = UILabel()
-        headerLabel.font = UIFont.systemFont(ofSize: 40)
-        headerLabel.textColor = UIColor(r: 173, g: 173, b: 173, alpha: 0.5)
-        headerLabel.text = "Gallery of Generative Art"
-        headerLabel.shadowColor = UIColor(r: 0, g: 0, b: 0, alpha: 0.1)
-        headerLabel.shadowOffset = CGSize(width: 0, height: 2)
-        headerContainerView.addSubview(headerLabel)
-        headerLabel.translatesAutoresizingMaskIntoConstraints = false
-        headerLabel.centerYAnchor.constraint(equalTo: headerContainerView.centerYAnchor).isActive = true
-        headerLabel.trailingAnchor.constraint(equalTo: headerContainerView.trailingAnchor, constant: -view.frame.size.height * 50 / 1119).isActive = true
-        
-        return headerContainerView
-    }()
-    
-    /// Sets up rotating line view of 60 duplicates.
-    private var rotatingLineView: UIView {
-        let view = UIView()
-        
-        let line = UIView(frame: CGRect(x: 0, y: 0, width: 3, height: 500))
-        view.addSubview(line)
-        
-        line.transform = CGAffineTransform(rotationAngle: -.pi / 4)
-        line.backgroundColor = UIColor(r: 255, g: 255, b: 255, alpha: 1)
-        line.alpha = 0.05
-        
-        line.loopInSuperview(duplicationCount: 60, with: [
-            .rotateByDegrees(-0.06),
-            .moveHorizontallyWithIncrement(30),
-            .updateOpacityIncreasingly
-            ]
-        )
-        
-        for subview in view.subviews {
-            subview.rotate(duration: 50.0)
-        }
-        
-        return view
-    }
-    
     // MARK: - Lifecycle functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.addSubview(headerView)
-        headerView.translatesAutoresizingMaskIntoConstraints = false
-        headerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        headerView.heightAnchor.constraint(equalToConstant: 140).isActive = true
-
+    
         let collectionView = verticalFlowCollectionView
         view.addSubview(collectionView)
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.topAnchor.constraint(equalTo: headerView.bottomAnchor).isActive = true
-        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        if let lineView = headerView.subviews.first(where: { $0.tag == rotatingLineViewTag }) {
-            for subview in lineView.subviews {
-                subview.rotate(duration: 30.0)
-            }
-        }
+        collectionView.constraint(edgesTo: view)
     }
 }
--- Gallery/Gallery tvOS/ArtPieceContainerViewController.swift
+++ Gallery/Gallery tvOS/ArtPieceContainerViewController.swift
@@ -26,87 +26,105 @@ class ArtPieceContainerViewController: UIViewController {
     /// It is set to featured art piece collection view selected cell during transition from `artPieceCollectionGridViewController` to `featuredArtPieceCollectionViewController`.
     private var artPieceCollectionGridViewControllerFocusGuide = UIFocusGuide()
     
+    private let loopingLinesView = LoopingLinesView()
+    private let headerView = UIView()
+    
     // MARK: - Lifecycle functions
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupBackgroundLayer()
+        setupHeaderView()
+        setupCollectionViewControllers()
         
+        NotificationCenter.default.addObserver(self, selector: #selector(startLineAnimation), name: .UIApplicationDidBecomeActive, object: nil)
+    }
+    
+    // MARK: - Background setup
+    
+    private func setupBackgroundLayer() {
+        let background = GradientBackgroundView(frame: view.bounds)
+        view.addSubview(background)
+    }
+    
+    private func setupHeaderView() {
+        headerView.addSubview(loopingLinesView)
+        loopingLinesView.translatesAutoresizingMaskIntoConstraints = false
+        loopingLinesView.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: view.frame.size.width * 300 / 1920).isActive = true
+        loopingLinesView.topAnchor.constraint(equalTo: headerView.topAnchor, constant: -view.frame.size.height * 50 / 1119).isActive = true
+        
+        let headerLabel = UILabel()
+        headerLabel.font = UIFont.systemFont(ofSize: 40)
+        headerLabel.textColor = UIColor(r: 173, g: 173, b: 173, alpha: 0.5)
+        headerLabel.text = "Gallery of Generative Art"
+        headerLabel.shadowColor = UIColor(r: 0, g: 0, b: 0, alpha: 0.1)
+        headerLabel.shadowOffset = CGSize(width: 0, height: 2)
+        
+        headerView.addSubview(headerLabel)
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        headerLabel.centerYAnchor.constraint(equalTo: headerView.centerYAnchor).isActive = true
+        headerLabel.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -view.frame.size.height * 50 / 1119).isActive = true
+        
+        view.addSubview(headerView)
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        headerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        headerView.heightAnchor.constraint(equalToConstant: 140).isActive = true
+    }
+    
+    private func setupCollectionViewControllers() {
         featuredArtPieceCollectionViewController.delegate = self
         artPieceCollectionGridViewController.delegate = self
-
+        
         addChildViewController(featuredArtPieceCollectionViewController)
         view.addSubview(featuredArtPieceCollectionViewController.view)
-
+        
         addChildViewController(artPieceCollectionGridViewController)
         view.addSubview(artPieceCollectionGridViewController.view)
-
+        
         featuredArtPieceCollectionViewController.view.translatesAutoresizingMaskIntoConstraints = false
         artPieceCollectionGridViewController.view.translatesAutoresizingMaskIntoConstraints = false
-
-        featuredArtPieceCollectionViewController.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        
+        featuredArtPieceCollectionViewController.view.topAnchor.constraint(equalTo: view.topAnchor, constant: 140).isActive = true
         featuredArtPieceCollectionViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         featuredArtPieceCollectionViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         featuredArtPieceCollectionViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -140).isActive = true
-
+        
         artPieceCollectionGridViewController.view.topAnchor.constraint(equalTo: featuredArtPieceCollectionViewController.view.bottomAnchor).isActive = true
         artPieceCollectionGridViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         artPieceCollectionGridViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         artPieceCollectionGridViewController.view.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
-
+        
         featuredArtPieceCollectionViewController.didMove(toParentViewController: self)
         artPieceCollectionGridViewController.didMove(toParentViewController: self)
-
+        
         artPieceCollectionGridViewController.view.addLayoutGuide(artPieceCollectionGridViewControllerFocusGuide)
-
+        
         artPieceCollectionGridViewControllerFocusGuide.topAnchor.constraint(equalTo: artPieceCollectionGridViewController.view.topAnchor).isActive = true
         artPieceCollectionGridViewControllerFocusGuide.leadingAnchor.constraint(equalTo: artPieceCollectionGridViewController.view.leadingAnchor).isActive = true
         artPieceCollectionGridViewControllerFocusGuide.trailingAnchor.constraint(equalTo: artPieceCollectionGridViewController.view.trailingAnchor).isActive = true
         artPieceCollectionGridViewControllerFocusGuide.heightAnchor.constraint(equalToConstant: 30).isActive = true
     }
     
-    // MARK: - Background setup
+    // MARK: - Animations
     
-    private func setupBackgroundLayer() {
-        view.backgroundColor = .white
-        
-        let linearGradient = CAGradientLayer()
-        view.layer.addSublayer(linearGradient)
-        linearGradient.frame = view.bounds
-        linearGradient.colors = [
-            UIColor(r: 186, g: 247, b: 253, alpha: 0.11).cgColor,
-            UIColor(r: 41, g: 56, b: 57, alpha: 0.65).cgColor,
-            UIColor(r: 0, g: 0, b: 0, alpha: 1.0).cgColor
-        ]
-        linearGradient.locations = [
-            NSNumber(value: 0),
-            NSNumber(value: 1 - 950 / 1119),
-            NSNumber(value: 1 - 1082 / 1119)
-        ]
-
-        let center = CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2)
-        let radius = view.frame.size.height * 790 / 1119
-        let colors = [UIColor(r: 186, g: 247, b: 253, alpha: 1).cgColor, UIColor(r: 255, g: 255, b: 255, alpha: 0).cgColor]
-        let radialGradient = RadialGradientLayer(startCenter: center,
-                                                 endCenter: center,
-                                                 startRadius: 0,
-                                                 endRadius: radius,
-                                                 colors: colors)
-        view.layer.addSublayer(radialGradient)
-        radialGradient.frame = view.bounds
-        radialGradient.opacity = 0.2
-        
-        let diagonalGradient = CAGradientLayer()
-        view.layer.addSublayer(diagonalGradient)
-        diagonalGradient.frame = view.bounds
-        diagonalGradient.opacity = 0.2
-        diagonalGradient.startPoint = CGPoint(x: 1, y: 0)
-        diagonalGradient.endPoint = CGPoint(x: 0, y: 1)
-        diagonalGradient.colors = [
-            UIColor(r: 186, g: 247, b: 253, alpha: 1).cgColor,
-            UIColor(r: 31, g: 50, b: 50, alpha: 1).cgColor
-        ]
+    @objc private func startLineAnimation() {
+        loopingLinesView.rotate()
+    }
+    
+    private func slideContentUp() {
+        featuredArtPieceCollectionViewController.view.transform = CGAffineTransform.identity
+        artPieceCollectionGridViewController.view.transform = CGAffineTransform.identity
+        headerView.transform = CGAffineTransform.identity
+    }
+    
+    private func slideContentDown() {
+        let translationY = featuredArtPieceCollectionViewController.view.bounds.size.height + 140
+        featuredArtPieceCollectionViewController.view.transform = CGAffineTransform(translationX: 0, y: -translationY)
+        artPieceCollectionGridViewController.view.transform = CGAffineTransform(translationX: 0, y: -translationY)
+        headerView.transform = CGAffineTransform(translationX: 0, y: -translationY)
     }
     
     // MARK: - UIFocusEnvironment update
@@ -130,8 +148,7 @@ class ArtPieceContainerViewController: UIViewController {
             artPieceCollectionGridViewControllerFocusGuide.preferredFocusEnvironments = []
             
             coordinator.addCoordinatedUnfocusingAnimations({ [weak self] (animator) in
-                self?.featuredArtPieceCollectionViewController.view.transform = CGAffineTransform.identity
-                self?.artPieceCollectionGridViewController.view.transform = CGAffineTransform.identity
+                self?.slideContentUp()
                 }, completion: nil)
             
             return
@@ -139,14 +156,11 @@ class ArtPieceContainerViewController: UIViewController {
         
         // Animates translation transform between view controllers.
         if featuredArtPieceCollectionViewController.contains(previouslyFocusedView), artPieceCollectionGridViewController.contains(nextFocusedView) {
-            
             // Sets preferred focus guide to `previouslyFocusedView`, so coming back to controller would focus last selected item.
             artPieceCollectionGridViewControllerFocusGuide.preferredFocusEnvironments = [previouslyFocusedView]
             
             coordinator.addCoordinatedUnfocusingAnimations({ [weak self] (animator) in
-                guard let translationY = self?.featuredArtPieceCollectionViewController.view.bounds.size.height else { return }
-                self?.featuredArtPieceCollectionViewController.view.transform = CGAffineTransform(translationX: 0, y: -translationY)
-                self?.artPieceCollectionGridViewController.view.transform = CGAffineTransform(translationX: 0, y: -translationY)
+                self?.slideContentDown()
                 }, completion: nil)
             
             return
