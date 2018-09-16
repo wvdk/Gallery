ce43fe565bbd005918d327d1d7e08ea0ef183649
Wes Van der Klomp <wes.vdk@icloud.com>
2018-09-16 12:41:55 -0700

    cleanup

--- Gallery/GalleryCore/MasterList.swift
+++ Gallery/GalleryCore/MasterList.swift
@@ -45,16 +45,7 @@ public class MasterList {
                         prettyPublishedDate: "June 2018",
                         description: "I made my art piece. I made it very pretty. And few more words.",
                         viewType: A565zView.self,
-                        thumbnail: UIImage(named: "A565z")),
-//            ArtMetadata(id: "A994t",
-//                        author: "Kristina Gelzinyte",
-//                        prettyPublishedDate: "June 2018",
-//                        description: "Lorem ipsum dolor sit amet, ligula suspendisse nulla pretium, rhoncus tempor fermentum, enim integer ad vestibulum volutpat. Nisl rhoncus turpis est, vel elit, congue wisi enim nunc ultricies sit, magna tincidunt. Maecenas aliquam. gna tincidunt. Maecenas aliquam tincidunt. Maecenas aliquam. Lorem ipsum dolor sit amet, ligula suspendisse nulla pretium, rhoncus tempor fermentum, enim integer ad vestibulum volutpat. Nisl rhoncus turpis est, vel elit, congue wisi enim nunc ultricies sit, magna tincidunt. Maecenas aliquam. gna tincidunt. Maecenas aliquam tincidunt. Maecenas aliquamorem ipsum dolor sit amet, ligula suspendisse nulla pretium, rhoncus tempor fermentum, enim integer ad vestibulum volutpat. Lorem ipsum dolor sit amet, ligula suspendisse nulla pretium, rhoncus tempor fermentum, enim integer ad vestibulum volutpat. Nisl rhoncus turpis est, vel elit, congue wisi enim nunc ultricies sit, magna tincidunt. Maecenas aliquam. gna tincidunt. Maecenas aliquam tincidunt. Maecenas aliquamorem ipsum dolor sit amet, ligula suspendisse nulla pretium, rhoncus tempor fermentum, enim integer ad vestibulum volutpat. END",
-//                        price: 99.99,
-//                        viewType: A994tView.self,
-//                        thumbnail: UIImage(named: "A994t")),
-//            ArtMetadata(id: "A586q", author: "wvdk", prettyPublishedDate: "June 2018", price: 0.99, viewType: A586qView.self, thumbnail: UIImage(named: "A586q")),
-//            ArtMetadata(id: "A736D", author: "wvdk", prettyPublishedDate: "June 2018", viewType: A736DView.self, thumbnail: UIImage(named: "A736D")),
+                        thumbnail: UIImage(named: "A565z"))
         ]
     }
     
