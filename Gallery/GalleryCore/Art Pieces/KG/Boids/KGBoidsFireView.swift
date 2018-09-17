cb8451fe38e3cfe7fbf40e8b6f06afa51c3ea297
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-09-17 09:46:11 -0700

    Added KGBoidsFireView

---
+++ Gallery/GalleryCore/Art Pieces/KG/Boids/KGBoidsFireView.swift
@@ -0,0 +1,42 @@
+//
+//  KGBoidsFireView.swift
+//  Gallery
+//
+//  Created by Kristina Gelzinyte on 9/17/18.
+//  Copyright © 2018 Gallery of Generative Art. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class KGBoidsFireView: ArtView {
+    
+    // MARK: - Properties
+    
+    private var scene = KGBoidsFireScene()
+    private let spriteKitView = SKView()
+    
+    // MARK: - Initialization
+    
+    public required init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(spriteKitView)
+        spriteKitView.translatesAutoresizingMaskIntoConstraints = false
+        spriteKitView.constraint(edgesTo: self)
+        
+        spriteKitView.ignoresSiblingOrder = true
+        spriteKitView.showsFPS = true
+        spriteKitView.showsNodeCount = true
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func willMove(toSuperview newSuperview: UIView?) {
+        super.willMove(toSuperview: newSuperview)
+        
+        spriteKitView.presentScene(scene)
+    }
+}
