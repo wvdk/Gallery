a09b34095ef8727f624341019c9bb99fdf42cb8e
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-09-18 11:06:37 -0700

    Added circular shape obstacle node

--- Gallery/GalleryCore/Art Pieces/KG/Boids/KGObstacleNode.swift
+++ Gallery/GalleryCore/Art Pieces/KG/Boids/KGObstacleNode.swift
@@ -10,23 +10,70 @@ import SpriteKit
 
 class KGObstacleNode: SKShapeNode {
     
-    // MARK: - Initialization
+    // MARK: - Constants
     static let uniqueName = "Obstacle"
+    
+    // MARK: - Obstacle option list
+    
+    enum Shape {
+        case rectangle
+        case circle
+    }
     
     // MARK: - Properties
     
-    private(set) var direction = CGVector.zero
+    private var rectangleDirection = CGVector.zero
+    private var shape: Shape = .rectangle
     
     // MARK: - Initialization
     
-    convenience init(frame: CGRect, direction: CGVector) {
-        self.init(rect: frame)
+    convenience init(rectangleOfFrame: CGRect, direction: CGVector) {
+        self.init(rect: rectangleOfFrame)
         self.name = KGObstacleNode.uniqueName
         
         self.fillColor = .clear
         self.strokeColor = .clear
-        //        self.alpha = 0
         
-        self.direction = direction
+        self.rectangleDirection = direction
+        self.shape = .rectangle
+    }
+    
+    convenience init(doghnutIn frame: CGRect) {
+        self.init(ellipseIn: frame)
+        
+        self.strokeColor = .white
+        self.lineWidth = 1
+        
+        self.shape = .circle
+    }
+    
+    // MARK: - Initialization
+    
+    func direction(at point: CGPoint = .zero) -> CGVector {
+        switch shape {
+            
+        case .rectangle:
+            return rectangleDirection
+            
+        case .circle:
+            let center = CGPoint(x: frame.midX, y: frame.midY)
+            let direction = CGVector(dx: center.x - point.x, dy: center.y - point.y)
+            let maxValue = max(abs(direction.dx), abs(direction.dy))
+            
+            if maxValue > 0 {
+                return direction.divide(by: maxValue)
+            }
+            
+            return .zero
+        }
+    }
+    
+    func intercepts(withBoid position: CGPoint) -> Bool {
+        switch shape {
+        case .rectangle:
+            return self.contains(position)
+        case .circle:
+            return !self.contains(position)
+        }
     }
 }
