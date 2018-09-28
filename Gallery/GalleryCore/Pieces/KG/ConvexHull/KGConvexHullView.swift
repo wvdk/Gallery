5c8f1fca6a14c4dcb0661a6c604cc94b8f2422b8
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-09-27 17:11:11 -0700

    KGConvexHullView init

---
+++ Gallery/GalleryCore/Pieces/KG/ConvexHull/KGConvexHullView.swift
@@ -0,0 +1,33 @@
+//
+//  KGConvexHullView.swift
+//  Gallery
+//
+//  Created by Kristina Gelzinyte on 9/27/18.
+//  Copyright © 2018 Gallery of Generative Art. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class KGConvexHullView: UIView {
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Initialization
+    
+    public required override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func willMove(toSuperview newSuperview: UIView?) {
+        super.willMove(toSuperview: newSuperview)
+        
+    }
+}
