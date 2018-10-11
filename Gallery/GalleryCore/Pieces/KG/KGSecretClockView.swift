4b2d6b333f677b7417096120708bb9dbc2d2a794
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-10-11 11:57:41 -0700

    Clock art piece

---
+++ Gallery/GalleryCore/Pieces/KG/KGSecretClockView.swift
@@ -0,0 +1,66 @@
+//
+//  KGSecretClockView.swift
+//  Gallery
+//
+//  Created by Kristina Gelzinyte on 10/11/18.
+//  Copyright © 2018 Gallery of Generative Art. All rights reserved.
+//
+
+import UIKit
+
+class KGSecretClockView : UIView {
+    
+    // MARK: - Properties
+    
+    private var timeLabel = UILabel()
+
+    // MARK: - Initialization
+    
+    public required override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .black
+        layer.opacity = 0.9
+        
+        setupTimeLabel()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func willMove(toSuperview newSuperview: UIView?) {
+        super.willMove(toSuperview: newSuperview)
+
+        showTimeLabel()
+    }
+    
+    // MARK: - Clock setup
+    
+    private func setupTimeLabel() {
+        let fontSize = 250 * frame.height / 1119
+        timeLabel.font = UIFont(name: "Menlo", size: fontSize)
+        timeLabel.textColor = .white
+        timeLabel.alpha = 0.5
+        
+        addSubview(timeLabel)
+        timeLabel.translatesAutoresizingMaskIntoConstraints = false
+        timeLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        timeLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        
+        updateTime()
+    }
+    
+    // MARK: - Clock appearance
+    
+    private func showTimeLabel() {
+        updateTime()
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+    }
+    
+    @objc private func updateTime() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH mm ss"
+        timeLabel.text = formatter.string(from: Date())
+    }
+}
