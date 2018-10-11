d04d99a63242888e8f213cdfd87d2924ef016431
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-10-11 11:57:16 -0700

    Removed clock from display controller

--- Gallery/Gallery tvOS/ArtPieceDisplayViewController.swift
+++ Gallery/Gallery tvOS/ArtPieceDisplayViewController.swift
@@ -18,7 +18,6 @@ class ArtPieceDisplayViewController: UIViewController {
     private let artContainerView = UIView()
 
     private var artPieceMetadata: PieceMetadata
-    private var timeLabel: UILabel?
     
     // MARK: - Initalization
     
@@ -57,78 +56,12 @@ class ArtPieceDisplayViewController: UIViewController {
         let menuTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(closeAction(_:)))
         menuTapGestureRecognizer.allowedPressTypes = [NSNumber(value: Int8(UIPress.PressType.menu.rawValue))]
         view.addGestureRecognizer(menuTapGestureRecognizer)
-        
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(showTimeLabel(_:)))
-        tapGestureRecognizer.allowedPressTypes = [NSNumber(value: Int8(UIPress.PressType.select.rawValue))]
-        view.addGestureRecognizer(tapGestureRecognizer)
     }
     
     // MARK: - Actions
 
     @objc private func closeAction(_ sender: UIButton) {
         delegate?.artPieceDisplayViewControllerDidSelectClose(self)
-    }
-    
-    @objc private func showTimeLabel(_ gestureRecognizer: UITapGestureRecognizer) {
-        if timeLabel == nil {
-            showClock()
-        } else {
-            hideClock()
-        }
-        
-        updateTime()
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
-    }
-    
-    // MARK: - Clock setup
-    
-    private func setupTimeLabel() {
-        timeLabel = UILabel()
-
-        guard let timeLabel = self.timeLabel else { return }
-        
-        let fontSize = 250 * view.frame.height / 1119
-        timeLabel.font = UIFont(name: "Menlo", size: fontSize)
-        timeLabel.textColor = .white
-        timeLabel.alpha = 0
-        
-        view.addSubview(timeLabel)
-        timeLabel.translatesAutoresizingMaskIntoConstraints = false
-        timeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        timeLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-    }
-    
-    // MARK: - Clock appearance
-    
-    private func showClock() {
-        setupTimeLabel()
-
-        guard let timeLabel = self.timeLabel else { return }
-        
-        UIView.animate(withDuration: 0.2) { [weak self] in
-            timeLabel.alpha = 0.4
-            self?.artContainerView.alpha = 0.4
-        }
-    }
-    
-    private func hideClock() {
-        guard let timeLabel = self.timeLabel else { return }
-
-        UIView.animate(withDuration: 0.2, animations: { [weak self] in
-            timeLabel.alpha = 0
-            self?.artContainerView.alpha = 1
-        }, completion: {[weak self]  _ in
-            timeLabel.removeFromSuperview()
-            self?.timeLabel = nil
-        })
-    }
-    
-    @objc private func updateTime() {
-        guard let timeLabel = self.timeLabel else { return }
-
-        let formatter = DateFormatter()
-        formatter.dateFormat = "HH mm ss"
-        timeLabel.text = formatter.string(from: Date())
     }
 }
 
