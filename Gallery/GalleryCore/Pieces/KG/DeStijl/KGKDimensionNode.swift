de2cc375e4f535483474eac05452fb97bd5541f4
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-10-24 08:46:12 +0300

    k dimension tree generator implementation

---
+++ Gallery/GalleryCore/Pieces/KG/DeStijl/KGKDimensionNode.swift
@@ -0,0 +1,210 @@
+//
+//  KGKDimensionNode.swift
+//  Gallery
+//
+//  Created by Kristina Gelzinyte on 10/24/18.
+//  Copyright © 2018 Gallery of Generative Art. All rights reserved.
+//
+
+import UIKit
+
+class KGKDimensionNode {
+    
+    private(set) var point: CGPoint
+    private(set) var left: KGKDimensionNode?
+    private(set) var right: KGKDimensionNode?
+    private(set) var parent: KGKDimensionNode?
+    
+    private(set) var dimension: Int
+    
+    private var startPoint: CGPoint?
+    private var endPoint: CGPoint?
+    
+    private var coordinate: CGFloat {
+        switch dimension {
+        case 1:
+            return point.x
+        default:
+            return point.y
+        }
+    }
+    
+    var lineMinPoint: CGPoint? {
+        switch dimension {
+        case 1:
+            if startPoint!.y < endPoint!.y {
+                return startPoint
+            }
+            return endPoint
+        default:
+            if startPoint!.x < endPoint!.x {
+                return startPoint
+            }
+            return endPoint
+        }
+    }
+    
+    var lineMaxPoint: CGPoint? {
+        switch dimension {
+        case 1:
+            if startPoint!.y > endPoint!.y {
+                return startPoint
+            }
+            return endPoint
+        default:
+            if startPoint!.x > endPoint!.x {
+                return startPoint
+            }
+            return endPoint
+        }
+    }
+    
+    @discardableResult func line(in frame: CGRect) -> KGLine {
+        startPoint = point
+        endPoint = point
+        
+        guard let parent = self.parent else {
+            switch dimension {
+            case 1:
+                startPoint?.y = frame.maxY
+                endPoint?.y = frame.minY
+            default:
+                startPoint?.x = frame.maxX
+                endPoint?.x = frame.minX
+            }
+            
+            return KGLine(startPoint: startPoint ?? point, endPoint: endPoint ?? point)
+        }
+        
+        endPoint = parent.point
+        
+        guard let grandparent = parent.parent else {
+            switch dimension {
+            case 1:
+                endPoint?.x = point.x
+                
+                if endPoint!.y < point.y {
+                    startPoint?.y = frame.maxY
+                } else {
+                    startPoint?.y = frame.minY
+                }
+            default:
+                endPoint?.y = point.y
+                
+                if endPoint!.x < point.x {
+                    startPoint?.x = frame.maxX
+                } else {
+                    startPoint?.x = frame.minX
+                }
+            }
+            return KGLine(startPoint: startPoint ?? point, endPoint: endPoint ?? point)
+        }
+        
+        switch dimension {
+        case 1:
+            endPoint?.x = point.x
+            
+            if startPoint!.y < parent.point.y {
+                startPoint?.y = grandparent.lineMinPoint?.y ?? point.y
+            } else {
+                startPoint?.y = grandparent.lineMaxPoint?.y ?? point.y
+            }
+            
+        default:
+            endPoint?.y = point.y
+            
+            if startPoint!.x < parent.point.x {
+                startPoint?.x = grandparent.lineMinPoint?.x ?? point.x
+            } else {
+                startPoint?.x = grandparent.lineMaxPoint?.x ?? point.x
+            }
+        }
+        
+        return KGLine(startPoint: startPoint ?? point, endPoint: endPoint ?? point)
+    }
+    
+    init(dimension: Int, point: CGPoint) {
+        self.dimension = dimension
+        self.point = point
+    }
+    
+    func update(right node: KGKDimensionNode) {
+        self.right = node
+    }
+    
+    func update(left node: KGKDimensionNode) {
+        self.left = node
+    }
+    
+    func update(parent: KGKDimensionNode) {
+        self.parent = parent
+    }
+    
+    func isLeft(_ neighborPoint: CGPoint) -> Bool {
+        switch dimension {
+        case 1:
+            return neighborPoint.x < point.x
+        default:
+            return neighborPoint.y < point.y
+        }
+    }
+    
+    func isRight(_ neighborPoint: CGPoint) -> Bool {
+        switch dimension {
+        case 1:
+            return neighborPoint.x > point.x
+        default:
+            return neighborPoint.y > point.y
+        }
+    }
+    
+    /// In sub-tree rooted at node, see if one of its descendants is closer to rawTarget than min[0].
+    func nearestPoint(to target: CGPoint, closerThan distance: CGFloat) -> CGPoint? {
+        var closestPoint: CGPoint? = nil
+        var minimumDistance = distance
+        
+        let newDistance = target.distance(to: point)
+        if distance > newDistance {
+            closestPoint = point
+            minimumDistance = newDistance
+        }
+        
+        // Evaluate if perpendicular distance to the axis along which node separates the plane needs to be computed for subtree nodes.
+        let perpendicularDistance = abs(coordinate - target.value(for: dimension))
+        
+        // If perpendicular distance is smaller than the smallest known distance, check both branches.
+        if perpendicularDistance < minimumDistance {
+            // Return closer one.
+            if let rightPoint = self.right?.nearestPoint(to: target, closerThan: minimumDistance), rightPoint.distance(to: target) < minimumDistance {
+                closestPoint = rightPoint
+            }
+            
+            if let leftPoint = self.left?.nearestPoint(to: target, closerThan: minimumDistance), leftPoint.distance(to: target) < minimumDistance {
+                closestPoint = leftPoint
+            }
+            
+        } else {
+            // Determine which branch to go.
+            if coordinate > target.value(for: dimension), let leftPoint = self.left?.nearestPoint(to: target, closerThan: minimumDistance), leftPoint.distance(to: target) < minimumDistance {
+                closestPoint = leftPoint
+                
+            } else if coordinate <= target.value(for: dimension), let rightPoint = self.right?.nearestPoint(to: target, closerThan: minimumDistance), rightPoint.distance(to: target) < minimumDistance {
+                closestPoint = rightPoint
+            }
+        }
+        
+        return closestPoint
+    }
+}
+
+fileprivate extension CGPoint {
+    
+    func value(for dimension: Int) -> CGFloat {
+        switch dimension {
+        case 1:
+            return x
+        default:
+            return y
+        }
+    }
+}
---
+++ Gallery/GalleryCore/Pieces/KG/DeStijl/KGKDimensionTree.swift
@@ -0,0 +1,143 @@
+//
+//  KGKDimensionTree.swift
+//  Gallery
+//
+//  Created by Kristina Gelzinyte on 10/24/18.
+//  Copyright © 2018 Gallery of Generative Art. All rights reserved.
+//
+
+import UIKit
+
+class KGKDimensionTree {
+    
+    // MARK: - Properties
+    
+    weak var delegate: KDTreeDelegate? = nil
+    
+    private(set) var root: KGKDimensionNode?
+    private var points: [CGPoint]!
+    private var maxDimension: Int!
+    
+    // MARK: - Initialization
+    
+    /// Recursively construct kd tree using median methods on input point.
+    init(maxDimension: Int, from points: [CGPoint]) {
+        guard !points.isEmpty else {
+            NSLog("0 points.")
+            return
+        }
+        
+        self.maxDimension = maxDimension
+        self.points = points
+    }
+    
+    func generate() {
+        guard let rootNode = generateKDNode(dimension: 1, left: 0, right: points.count - 1) else {
+            NSLog("No node available.")
+            return
+        }
+        
+        self.root = rootNode
+    }
+    
+    // MARK: - KD node initialization
+    
+    /// KDNode recursive genration for the KDTree.
+    private func generateKDNode(dimension: Int, left: Int, right: Int) -> KGKDimensionNode? {
+        guard right >= left else {
+            NSLog("Wrong range - right < left")
+            return nil
+        }
+        
+        if right == left {
+            let node = KGKDimensionNode(dimension: dimension, point: points[left])
+            delegate?.kdTree(self, didAdd: node)
+            return node
+        }
+        
+        // Order the array of points so the mth element will be the median and the elements prior to it will be all <=, though they won't be sorted; similarly, the elements after will be all >=.
+        let medium = 1 + (right - left) / 2
+        
+        let algorithm = KGQuicksortSortingAlgorithm(sortingArray: points, dimension: dimension)
+        algorithm.select(mediumIndex: medium, leftIndex: left, rightIndex: right)
+        points = algorithm.sortingArray
+        
+        // Median point becomes the parent.
+        let parentNode = KGKDimensionNode(dimension: dimension, point: points[left + medium - 1])
+        delegate?.kdTree(self, didAdd: parentNode)
+        
+        // Update the next dimesnion or reset back to 1.
+        
+        var updatedDimension = dimension + 1
+        if updatedDimension > maxDimension {
+            updatedDimension = 1
+        }
+        
+        if let rightNode = generateKDNode(dimension: updatedDimension, left: left + medium, right: right) {
+            rightNode.update(parent: parentNode)
+            parentNode.update(right: rightNode)
+        }
+        
+        if let leftNode = generateKDNode(dimension: updatedDimension, left: left, right: left + medium - 2) {
+            leftNode.update(parent: parentNode)
+            parentNode.update(left: leftNode)
+        }
+        
+        return parentNode
+    }
+    
+    // MARK: - Nearest node finding
+    
+    private func parent(for point: CGPoint) -> KGKDimensionNode? {
+        guard let root = self.root else {
+            NSLog("No parent detected.")
+            return nil
+        }
+        
+        // Go through tree iteratively, varying from vertical to horizontal.
+        var parent: KGKDimensionNode? = root
+        
+        while true {
+            // If point is left node, search that branch.
+            if parent!.isLeft(point), let leftNode = parent?.left {
+                parent = leftNode
+                
+                // If point is right node, search that branch.
+            } else if parent!.isRight(point), let rightNode = parent?.right {
+                parent = rightNode
+                
+                // Last node, return parent.
+            } else {
+                return parent
+            }
+        }
+    }
+    
+    func nearestNeighbor(for point: CGPoint) -> CGPoint? {
+        guard let root = root else {
+            NSLog("No neighbors detected.")
+            return nil
+        }
+        
+        var smallestDistance = CGFloat.infinity
+        var nearestNeighborPoint: CGPoint?
+        
+        // Find parent node to which neighbor would have been inserted. This is our best shot at locating the closest point. Compute best distance.
+        if let parentNodePoint = parent(for: point)?.point {
+            smallestDistance = point.distance(to: parentNodePoint)
+            nearestNeighborPoint = parentNodePoint
+        }
+        
+        // Check all rectangles that potentially overlap this smallest distance. If better is found, return it.
+        if let closerPoint = root.nearestPoint(to: point, closerThan: smallestDistance) {
+            nearestNeighborPoint = closerPoint
+        }
+        
+        return nearestNeighborPoint
+    }
+}
+
+protocol KDTreeDelegate: class {
+    
+    func kdTree(_ tree: KGKDimensionTree, didAdd node: KGKDimensionNode)
+}
