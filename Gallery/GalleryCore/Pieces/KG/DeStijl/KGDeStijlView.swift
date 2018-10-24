92c303aff5da2c09aa4d6af144a55e1063e9e0f1
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-10-24 07:49:34 +0300

    De Stijl art piece init

---
+++ Gallery/GalleryCore/Pieces/KG/DeStijl/KGDeStijlView.swift
@@ -0,0 +1,13 @@
+//
+//  KGDeStijlView.swift
+//  Gallery
+//
+//  Created by Kristina Gelzinyte on 10/24/18.
+//  Copyright © 2018 Gallery of Generative Art. All rights reserved.
+//
+
+import UIKit
+
+class KGDeStijlView: UIView {
+    
+}
