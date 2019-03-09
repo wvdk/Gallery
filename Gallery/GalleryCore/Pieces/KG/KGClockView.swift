645ae29bdee7b7256a2fe6d18fed1e8f71c289f3
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2019-03-09 17:22:04 +0200

    Updated clock appearance

--- Gallery/GalleryCore/Pieces/KG/KGClockView.swift
+++ Gallery/GalleryCore/Pieces/KG/KGClockView.swift
@@ -54,59 +54,11 @@ class KGClockView: UIView {
         
         super.init(frame: frame)
         
-        backgroundColor = UIColor(r: 74, g: 68, b: 68)
-        
-        secondsLayer.backgroundColor = UIColor(r: 196, g: 93, b: 105).cgColor
-        minutesLayer.backgroundColor = UIColor(r: 180, g: 180, b: 180).cgColor
-        hoursLayer.backgroundColor = UIColor(r: 180, g: 180, b: 180).cgColor
-        
-        let w = (frame.size.width + frame.size.height) * 0.3
-        
-        secondsLayer.anchorPoint = CGPoint(x: 0.5, y: 0.95)
-        minutesLayer.anchorPoint = CGPoint(x: 0.5, y: 1 - 13 / (w / 2))
-        hoursLayer.anchorPoint = CGPoint(x: 0.5, y: 1 - 13 / (w / 2))
-
-        secondsLayer.frame = CGRect(x: 0, y: 0, width: 4, height: 20 + w / 2)
-        minutesLayer.frame = CGRect(x: 0, y: 0, width: 20, height: w / 2)
-        hoursLayer.frame = CGRect(x: 0, y: 0, width: 20, height: w / 3)
-        
-        secondsLayer.cornerRadius = 2
-        minutesLayer.cornerRadius = 10
-        hoursLayer.cornerRadius = 10
-        
-        secondsLayer.shadowOffset = .zero
-        minutesLayer.shadowOffset = .zero
-        hoursLayer.shadowOffset = .zero
-        
-        secondsLayer.shadowRadius = 1
-        minutesLayer.shadowRadius = 1
-        hoursLayer.shadowRadius = 1
-        
-        secondsLayer.shadowOpacity = 0.15
-        minutesLayer.shadowOpacity = 0.15
-        hoursLayer.shadowOpacity = 0.15
-
-        let centerPoint = CGPoint(x: frame.origin.x + frame.size.width / 2, y: frame.origin.y + frame.size.height / 2)
-        secondsLayer.position = centerPoint
-        minutesLayer.position = centerPoint
-        hoursLayer.position = centerPoint
-        
-        let pinLayer = CALayer()
-        pinLayer.backgroundColor = UIColor(r: 57, g: 54, b: 54).cgColor
-        pinLayer.frame = CGRect(x: 0, y: 0, width: 11, height: 11)
-        pinLayer.position = centerPoint
-        pinLayer.cornerRadius = 7
-        
-        self.layer.addSublayer(hoursLayer)
-        self.layer.addSublayer(minutesLayer)
-        self.layer.addSublayer(secondsLayer)
-        self.layer.addSublayer(pinLayer)
+        configureBackground(frame: frame)
+        configureArrows(frame: frame)
+        configureNumbers(frame: frame)
         
         updateTime()
-        
-        let numbers = KGClockNumberView(frame: frame)
-        self.addSubview(numbers)
-        numbers.constraint(edgesTo: numbers)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -123,6 +75,107 @@ class KGClockView: UIView {
         }
     }
     
+    // MARK: - View
+    
+    private func configureBackground(frame: CGRect) {
+        let background = UIView()
+        background.backgroundColor = UIColor(r: 74, g: 74, b: 74)
+        
+        let circle = CAShapeLayer()
+        let length = 0.375 * frame.width
+        let rect = CGRect(x: (frame.width - length) * 0.5, y: (frame.height - length) * 0.5, width: length, height: length)
+        circle.path = UIBezierPath(ovalIn: rect).cgPath
+        circle.fillColor = UIColor.white.cgColor
+        
+        let bottomPinLayer = CALayer()
+        bottomPinLayer.backgroundColor = UIColor(r: 74, g: 74, b: 74).cgColor
+        bottomPinLayer.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+        bottomPinLayer.position = CGPoint(x: frame.origin.x + frame.size.width / 2, y: frame.origin.y + frame.size.height / 2)
+        bottomPinLayer.cornerRadius = 12
+        
+        background.layer.addSublayer(circle)
+        background.layer.addSublayer(bottomPinLayer)
+
+        self.addSubview(background)
+        background.constraint(edgesTo: self)
+    }
+    
+    private func configureArrows(frame: CGRect) {
+        secondsLayer.backgroundColor = UIColor(r: 196, g: 93, b: 105).cgColor
+        minutesLayer.backgroundColor = UIColor(r: 74, g: 74, b: 74).cgColor
+        hoursLayer.backgroundColor = UIColor(r: 74, g: 74, b: 74).cgColor
+        
+        secondsLayer.anchorPoint = CGPoint(x: 0.5, y: 0.9)
+        minutesLayer.anchorPoint = CGPoint(x: 0.5, y: 1)
+        hoursLayer.anchorPoint = CGPoint(x: 0.5, y: 1)
+        
+        let w = frame.width * 0.356
+        secondsLayer.frame = CGRect(x: 0, y: 0, width: 4, height: w * 0.55)
+        minutesLayer.frame = CGRect(x: 0, y: 0, width: 12, height: w * 0.5)
+        hoursLayer.frame = CGRect(x: 0, y: 0, width: 12, height: w * 0.33)
+        
+        secondsLayer.cornerRadius = 2
+        minutesLayer.cornerRadius = 10
+        hoursLayer.cornerRadius = 10
+        
+        secondsLayer.shadowOffset = .zero
+        minutesLayer.shadowOffset = .zero
+        hoursLayer.shadowOffset = .zero
+        
+        secondsLayer.shadowRadius = 1
+        minutesLayer.shadowRadius = 1
+        hoursLayer.shadowRadius = 1
+        
+        secondsLayer.shadowOpacity = 0.15
+        minutesLayer.shadowOpacity = 0.15
+        hoursLayer.shadowOpacity = 0.15
+        
+        let centerPoint = CGPoint(x: frame.origin.x + frame.size.width / 2, y: frame.origin.y + frame.size.height / 2)
+        secondsLayer.position = centerPoint
+        minutesLayer.position = centerPoint
+        hoursLayer.position = centerPoint
+        
+        let pinLayer = CALayer()
+        pinLayer.backgroundColor = UIColor(r: 196, g: 93, b: 105).cgColor
+        pinLayer.frame = CGRect(x: 0, y: 0, width: 10, height: 10)
+        pinLayer.position = centerPoint
+        pinLayer.cornerRadius = 5
+        
+        let view = UIView()
+        self.addSubview(view)
+        view.constraint(edgesTo: self)
+        
+        view.layer.addSublayer(hoursLayer)
+        view.layer.addSublayer(minutesLayer)
+        view.layer.addSublayer(secondsLayer)
+        view.layer.addSublayer(pinLayer)
+    }
+    
+    private func configureNumbers(frame: CGRect) {
+        let view = UIView()
+        self.addSubview(view)
+        view.constraint(edgesTo: self)
+        
+        func angle(for hour: CGFloat) -> CGFloat {
+            return .pi * hour / 6
+        }
+        
+        for index in 1...12 {
+            let number = UILabel()
+            number.textColor = UIColor(r: 74, g: 74, b: 74)
+            number.font = UIFont.systemFont(ofSize: 50, weight: .bold)
+            number.text = "\(index)"
+            number.sizeToFit()
+            
+            let w = 0.16 * frame.width
+            let x = frame.size.width / 2 +  w * sin(angle(for: CGFloat(index)))
+            let y = frame.size.height / 2 - w * cos(angle(for: CGFloat(index)))
+            number.center = CGPoint(x: x, y: y)
+            
+            view.addSubview(number)
+        }
+    }
+    
     // MARK: - Time update
 
     @objc private func updateTime() {
@@ -133,38 +186,5 @@ class KGClockView: UIView {
         minutesLayer.transform = CATransform3DMakeRotation(minuteAngle, 0, 0, 1)
         hoursLayer.transform = CATransform3DMakeRotation(hourAngle, 0, 0, 1)
         CATransaction.commit()
-    }
-}
-
-class KGClockNumberView: UIView {
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        setupNumbers(in: frame)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupNumbers(in frame: CGRect) {
-        for index in 1...12 {
-            let number = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-            number.textColor = UIColor.white.withAlphaComponent(0.3)
-            number.text = "\(index)"
-            number.sizeToFit()
-            
-            let w = (frame.size.width + frame.size.height) * 0.14
-            let x = frame.size.width / 2 +  w * sin(angle(for: CGFloat(index)))
-            let y = frame.size.height / 2 - w * cos(angle(for: CGFloat(index)))
-            number.center = CGPoint(x: x, y: y)
-            
-            self.addSubview(number)
-        }
-    }
-    
-    private func angle(for hour: CGFloat) -> CGFloat {
-        return .pi * hour / 6
     }
 }
