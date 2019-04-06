747838e3819cc0624a1a593a445636fc8c915f33
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2019-04-06 15:22:16 +0300

    New grass node

--- Gallery/GalleryCore/Pieces/KG/Aquarium/KGAquariumScene.swift
+++ Gallery/GalleryCore/Pieces/KG/Aquarium/KGAquariumScene.swift
@@ -10,24 +10,19 @@ import SpriteKit
 
 class KGAquariumScene: SKScene {
     
+    static let waterZ: CGFloat = 0
+    static let bubbleZ: CGFloat = 12
+    static let fishZ: CGFloat = 12
+    static let waterGrassZ: CGFloat = 22
+    
     private let water = SKSpriteNode(imageNamed: "KGAquarium/Water")
-    private let grassFront = SKSpriteNode(imageNamed: "KGAquarium/Grass1")
-    private let grassBack = SKSpriteNode(imageNamed: "KGAquarium/Grass2")
-
-    private let waterZ: CGFloat = 0
-    private let bubbleZ: CGFloat = 12
-    private let fishZ: CGFloat = 12
-
+    private let waterGrass = KGWaterGrassNode()
+    
     override init(size: CGSize) {
         super.init(size: size)
         
         addChild(water)
-        addChild(grassFront)
-        addChild(grassBack)
-        
-        water.zPosition = waterZ
-        grassFront.zPosition = fishZ + 10
-        grassBack.zPosition = grassFront.zPosition - 1
+        addChild(waterGrass)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -40,23 +35,16 @@ class KGAquariumScene: SKScene {
         self.size = view.superview?.frame.size ?? UIScreen.main.nativeBounds.size
 
         water.size = size
-        grassFront.size = size
-        grassBack.size = size
+        water.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        water.zPosition = KGAquariumScene.waterZ
 
-        let center = CGPoint(x: size.width / 2, y: size.height / 2)
-
-        water.position = center
-        grassFront.position = center
-        grassBack.position = center
-
-        configureGrassFrontSwing()
-        configureGrassBackSwing()
+        waterGrass.configure(withSize: size)
 
         for _ in 0...5 {
             configureFish()
         }
         
-       configureBubbles()
+        configureBubbles()
     }
     
     // Called before each frame is rendered
@@ -65,7 +53,7 @@ class KGAquariumScene: SKScene {
     
     private func configureFish(){
         let fish = KGFishNode()
-        fish.zPosition = fishZ + CGFloat.random(in: 0...1)
+        fish.zPosition = KGAquariumScene.fishZ + CGFloat.random(in: 0...1)
 
         let scaleConstant = CGFloat.random(in: 0.25...0.4)
         fish.size.height *= scaleConstant
@@ -88,7 +76,7 @@ class KGAquariumScene: SKScene {
         
         bubbles.position = CGPoint(x: size.width / 2, y: 0)
         bubbles.particlePositionRange.dx = size.width / 2
-        bubbles.zPosition = bubbleZ
+        bubbles.zPosition = KGAquariumScene.bubbleZ
         bubbles.alpha = 0.2
         bubbles.particleLifetime = 10.0
         bubbles.particleBirthRate = 1
@@ -96,91 +84,4 @@ class KGAquariumScene: SKScene {
         addChild(bubbles)
     }
     
-    private func configureGrassFrontSwing(){
-        let sourcePositions: [float2] = [
-            float2(0, 1),       float2(0.5, 1),     float2(1, 1),
-            float2(0, 0.6),     float2(0.5, 0.6),   float2(1, 0.6),
-            float2(0, 0),       float2(0.5, 0),     float2(1, 0)
-        ]
-        
-        let destinationPositions1: [float2] = [
-            float2(0.05, 0.99),  float2(0.55, 0.98),  float2(1.05, 0.96),
-            float2(0.01, 0.59),  float2(0.51, 0.58),  float2(1.01, 0.56),
-            float2(0, 0),        float2(0.5, 0),      float2(1, 0)
-        ]
-        
-        let destinationPositions2: [float2] = [
-            float2(0.1, 0.98),   float2(0.6, 0.96),   float2(1.1, 0.92),
-            float2(0.02, 0.58),  float2(0.52, 0.56),  float2(1.02, 0.52),
-            float2(0, 0),        float2(0.5, 0),      float2(1, 0)
-        ]
-        
-        let destinationPositions3: [float2] = [
-            float2(-0.04, 1),   float2(0.46, 1),    float2(0.97, 1),
-            float2(0, 0.6),     float2(0.5, 0.6),   float2(1, 0.6),
-            float2(0, 0),       float2(0.5, 0),     float2(1, 0)
-        ]
-        
-        let warpGeometryGrid1 = SKWarpGeometryGrid(columns: 2, rows: 2,
-                                                   sourcePositions: sourcePositions,
-                                                   destinationPositions: destinationPositions1)
-        let warpGeometryGrid2 = SKWarpGeometryGrid(columns: 2, rows: 2,
-                                                   sourcePositions: sourcePositions,
-                                                   destinationPositions: destinationPositions2)
-        let warpGeometryGrid3 = SKWarpGeometryGrid(columns: 2, rows: 2,
-                                                   sourcePositions: sourcePositions,
-                                                   destinationPositions: destinationPositions3)
-        
-        let time = drand48() + 1.0
-        let swingAction = SKAction.animate(withWarps:[warpGeometryGrid1, warpGeometryGrid2, warpGeometryGrid3],
-                                           times: [NSNumber(value: time), NSNumber(value: time * 2), NSNumber(value: time * 3)])
-        let warpAction = SKAction.repeatForever(swingAction!)
-        grassFront.run(warpAction)
-    }
-    
-    private func configureGrassBackSwing(){
-        let sourcePositions: [float2] = [
-            float2(0, 1),       float2(0.5, 1),     float2(1, 1),
-            float2(0, 0.8),     float2(0.5, 0.8),   float2(1, 0.8),
-            float2(0, 0.4),     float2(0.5, 0.4),   float2(1, 0.4),
-            float2(0, 0),       float2(0.5, 0),     float2(1, 0)
-        ]
-        
-        let destinationPositions1: [float2] = [
-            float2(-0.05, 1.05),   float2(0.45, 1.1),    float2(1.05, 1),
-            float2(-0.03, 0.79),   float2(0.47, 0.82),   float2(1.03, 0.85),
-            float2(0, 0.35),       float2(0.54, 0.3),    float2(0.95, 0.45),
-            float2(0, -0.1),       float2(0.3, 0.1),     float2(1.1, 0.1)
-        ]
-        
-        let destinationPositions2: [float2] = [
-            float2(0.1, 0.98),    float2(0.6, 0.99),   float2(1.1, 0.92),
-            float2(0.06, 0.78),   float2(0.5, 0.76),  float2(1.06, 0.8),
-            float2(0.01, 0.4),    float2(0.51, 0.3),  float2(1.01, 0.38),
-            float2(0, 0),         float2(0.5, 0),      float2(1, 0)
-        ]
-        
-        let destinationPositions3: [float2] = [
-            float2(0.04, 1),     float2(0.46, 1.1),   float2(0.97, 1.1),
-            float2(0.04, 0.8),     float2(0.47, 0.8),   float2(0.98, 0.8),
-            float2(0.02, 0.4),      float2(0.4, 0.4),    float2(1.06, 0.4),
-            float2(0, 0),           float2(0.5, 0),      float2(1, 0)
-        ]
-        
-        let warpGeometryGrid1 = SKWarpGeometryGrid(columns: 2, rows: 3,
-                                                   sourcePositions: sourcePositions,
-                                                   destinationPositions: destinationPositions1)
-        let warpGeometryGrid2 = SKWarpGeometryGrid(columns: 2, rows: 3,
-                                                   sourcePositions: sourcePositions,
-                                                   destinationPositions: destinationPositions2)
-        let warpGeometryGrid3 = SKWarpGeometryGrid(columns: 2, rows: 3,
-                                                   sourcePositions: sourcePositions,
-                                                   destinationPositions: destinationPositions3)
-        
-        let time = drand48() + 1.0
-        let swingAction = SKAction.animate(withWarps:[warpGeometryGrid1, warpGeometryGrid2, warpGeometryGrid3],
-                                           times: [NSNumber(value: time), NSNumber(value: time * 2), NSNumber(value: time * 3)])
-        let warpAction = SKAction.repeatForever(swingAction!)
-        grassBack.run(warpAction)
-    }
 }
